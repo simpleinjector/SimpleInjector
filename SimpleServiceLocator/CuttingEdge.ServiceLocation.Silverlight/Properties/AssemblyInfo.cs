fa635812912efe1301f180ec4a97411a0d71aec3
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2010-09-06 13:31:56 -0700

    AssemblyInfo.cs of Silverlight project was missing from source control.

---
+++ SimpleServiceLocator/CuttingEdge.ServiceLocation.Silverlight/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CuttingEdge.ServiceLocation")]
+[assembly: AssemblyDescription("A simple but complete implementation of the Common Service Locator interface for Sylverlight.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cutting Edge Internet Technologies")]
+[assembly: AssemblyProduct("CuttingEdge.ServiceLocation")]
+[assembly: AssemblyCopyright("Copyright © 2010 S. van Deursen")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("35428847-e129-4fd5-8d36-1aae55b69ba5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
