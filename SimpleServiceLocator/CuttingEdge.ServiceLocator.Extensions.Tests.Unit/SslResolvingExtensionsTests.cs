e3ee3d41aec666e0aeec34bd8c79e4e3a7b82093
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-02-19 02:46:04 -0800

    TryGetInstance extension method based on the IServiceProvider interface added to the CuttingEdge.ServiceLocator.Extensions project.

---
+++ SimpleServiceLocator/CuttingEdge.ServiceLocator.Extensions.Tests.Unit/SslResolvingExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using CuttingEdge.ServiceLocation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CuttingEdge.ServiceLocator.Extensions.Tests.Unit
+{
+    [TestClass]
+    public class SslResolvingExtensionsTests
+    {
+        [TestMethod]
+        public void TryGetInstance_ServiceTypeRegistered_ReturnsTrue()
+        {
+            // Arrange
+            var container = new SimpleServiceLocator();
+
+            container.RegisterSingle<IWeapon>(new Katana());
+
+            // Act
+            IWeapon weapon;
+
+            bool found = container.TryGetInstance(out weapon);
+
+            // Assert
+            Assert.IsTrue(found, "TryGetInstance is expected to return true when the type is registered.");
+        }
+
+        [TestMethod]
+        public void TryGetInstance_ServiceTypeRegistered_ReturnsInstance()
+        {
+            // Arrange
+            var container = new SimpleServiceLocator();
+
+            container.RegisterSingle<IWeapon>(new Katana());
+
+            // Act
+            IWeapon weapon;
+
+            container.TryGetInstance<IWeapon>(out weapon);
+
+            // Assert
+            Assert.IsNotNull(weapon, "TryGetInstance is expected to return an instance when the type is registered.");
+        }
+
+        [TestMethod]
+        public void TryGetInstance_ServiceTypeNotRegistered_ReturnsFalse()
+        {
+            // Arrange
+            var container = new SimpleServiceLocator();
+
+            // Act
+            IWeapon weapon;
+
+            bool found = container.TryGetInstance(out weapon);
+
+            // Assert
+            Assert.IsFalse(found, "TryGetInstance is expected to return false when the type is not registered.");
+        }
+
+        [TestMethod]
+        public void TryGetInstance_ServiceTypeNotRegistered_ReturnsNull()
+        {
+            // Arrange
+            var container = new SimpleServiceLocator();
+
+            // Act
+            IWeapon weapon;
+
+            container.TryGetInstance(out weapon);
+
+            // Assert
+            Assert.IsNull(weapon, "TryGetInstance is expected to return null when the type is not registered.");
+        }
+    }
+}
