76a872fd9581162af38d6f7d422724efbe622b79
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-05-28 04:32:09 -0700

    RegisterWithConstructor extension method + tests added in CodeSamples project.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/ConstructorRegistrationExtensionsTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    [TestClass]
+    public class ConstructorRegistrationExtensionsTests
+    {
+        [TestMethod]
+        public void RegisterWithConstructor_WithValidArgument_Succeeds()
+        {
+            // Arrange
+            var container = new Container();
+            
+            // Act
+            container.RegisterWithConstructor<ICommand>(() => new MultipleConstructorsCommand((ILogger)null));
+        }
+
+        [TestMethod]
+        public void GetInstance_CalledAfterValidRegistration_Succeeds()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterSingle<ILogger, NullLogger>();
+            container.RegisterWithConstructor<ICommand>(() => new MultipleConstructorsCommand((ILogger)null));
+
+            // Act
+            var command = container.GetInstance<ICommand>();
+        }
+
+        private sealed class NullLogger : ILogger
+        {
+            public void Log(string message)
+            {
+            }
+        }
+
+        private sealed class MultipleConstructorsCommand : ICommand
+        {
+            public MultipleConstructorsCommand()
+            {
+            }
+
+            public MultipleConstructorsCommand(ILogger logger)
+            {
+            }
+
+            public MultipleConstructorsCommand(ICommand command)
+            {
+            }
+
+            public MultipleConstructorsCommand(ILogger logger, ICommand command)
+            {
+            }
+
+            public void Execute()
+            {
+            }
+        }
+    }
+}
---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/ConstructorRegistrationExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    public static class ConstructorRegistrationExtensions
+    {
+        public static void RegisterWithConstructor<TService>(this Container container, 
+            Expression<Func<TService>> constructorCall)
+            where TService : class
+        {
+            var ctor = ((NewExpression)constructorCall.Body).Constructor;
+
+            Func<TService> instanceCreator = null;
+
+            container.Register<TService>(() =>
+            {
+                if (instanceCreator == null)
+                {
+                    instanceCreator = Build<TService>(container, ctor);
+                }
+
+                return instanceCreator();
+            });
+        }
+
+        [DebuggerStepThrough]
+        private static Func<TService> Build<TService>(Container container, 
+            ConstructorInfo constructor)
+        {
+            var parameters =
+                from parameter in constructor.GetParameters()
+                select BuildParameter(container, parameter.ParameterType);
+
+            var newExpression = Expression.Lambda<Func<TService>>(
+                Expression.New(constructor, parameters.ToArray()), 
+                new ParameterExpression[0]);
+
+            return newExpression.Compile();
+        }
+
+        [DebuggerStepThrough]
+        private static Expression BuildParameter(Container container,
+            Type parameterType)
+        {
+            var instanceProducer = container.GetRegistration(parameterType);
+
+            if (instanceProducer == null)
+            {
+                container.GetInstance(parameterType);
+            }
+
+            return instanceProducer.BuildExpression();
+        }
+    }
+}
