b4aa1f857cd28ca37207bb06d33b68e0b926571b
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-11-18 08:29:12 -0800

    Extension methods for context dependent injection added to the CodeSamples project to show case the just added Container.Intercepting event.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/ContextDependentExtensionsTests.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    [TestClass]
+    public class ContextDependentExtensionsTests
+    {
+        public interface IContext
+        {
+        }
+
+        public interface ICommandHandler<TCommand>
+        {
+            void Execute(TCommand command);
+        }
+
+        [TestMethod]
+        public void GetInstance_ResolvingAConcreteTypeThatDependsOnAContextDependentType_InjectsExpectedType()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ImplementationType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // Act
+            // Note: IntCommandHandler depends on IContext.
+            var handler = container.GetInstance<IntCommandHandler>();
+
+            // Assert
+            Assert.IsInstanceOfType(handler.InjectedContext, typeof(CommandHandlerContext<IntCommandHandler>));
+        }
+
+        [TestMethod]
+        public void GetInstance_ResolvingAConcreteTypeThatDependsOnAContextDependentType_InjectsExpectedType2()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                // Now we use the ServiceType instead of the ImplementationType.
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ServiceType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // Act
+            // IntCommandHandler depends on IContext.
+            var handler = container.GetInstance<IntCommandHandler>();
+
+            // Assert
+            // Because we requested IntCommandHandler directly, the dc.ServiceType is of type IntCommandHandler.
+            Assert.IsInstanceOfType(handler.InjectedContext, typeof(CommandHandlerContext<IntCommandHandler>));
+        }
+
+        [TestMethod]
+        public void GetInstance_ResolvingAnInterfaceWhosImplementationDependsOnAContextDependentType_InjectsExpectedType()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ImplementationType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // Note: IntCommandHandler depends on IContext.
+            container.Register<ICommandHandler<int>, IntCommandHandler>();
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<int>>() as IntCommandHandler;
+
+            // Assert.IsInstanceOfType failed.  
+            // Expected type:<CommandHandlerContext`1[IntCommandHandler]>. 
+            // Actual type:<CommandHandlerContext`1[ICommandHandler`1[System.Int32]]>.
+
+            // Assert
+            Assert.IsInstanceOfType(handler.InjectedContext, typeof(CommandHandlerContext<IntCommandHandler>));
+        }
+
+        [TestMethod]
+        public void GetInstance_ResolvingAnInterfaceWhosImplementationDependsOnAContextDependentType_InjectsExpectedType2()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                // Now we use the ServiceType instead of the ImplementationType.
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ServiceType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // IntCommandHandler depends on IContext.
+            container.Register<ICommandHandler<int>, IntCommandHandler>();
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<int>>() as IntCommandHandler;
+
+            // Assert
+            Assert.IsInstanceOfType(handler.InjectedContext, typeof(CommandHandlerContext<ICommandHandler<int>>));
+        }
+
+        [TestMethod]
+        public void GetInstance_CalledDirectlyOnTheContextDependentType_InjectsADependencyContextWithoutServiceType()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                // Assert
+                Assert.IsNull(dc.ServiceType);
+
+                return new CommandHandlerContext<object>();
+            });
+
+            // Act
+            container.GetInstance<IContext>();
+        }
+
+        [TestMethod]
+        public void GetInstance_CalledDirectlyOnTheContextDependentType_InjectsADependencyContextWithoutImplementationType()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                // Assert
+                Assert.IsNull(dc.ImplementationType);
+
+                return new CommandHandlerContext<object>();
+            });
+
+            // Act
+            container.GetInstance<IContext>();
+        }
+
+        [TestMethod]
+        public void GetInstance_TypeWithContextRegisteredMultipleLevelsDeep_GetsInjectedWithExpectedContext()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ImplementationType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // IntCommandHandler depends on IContext.
+            container.Register<ICommandHandler<int>, IntCommandHandler>();
+
+            // Act
+            // CommandHandlerWrapper<T> depends on ICommandHandler<T>
+            var wrapper = container.GetInstance<CommandHandlerWrapper<int>>();
+
+            // Assert
+            var handler = (IntCommandHandler)wrapper.InjectedHandler;
+
+            Assert.IsInstanceOfType(handler.InjectedContext, typeof(CommandHandlerContext<IntCommandHandler>));
+        }
+
+        [TestMethod]
+        public void GetInstance_TypeWithContextRegisteredAtMultipleLevels_GetsInjectedWithExpectedContext()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterWithContext<IContext>(dc =>
+            {
+                var contextType = typeof(CommandHandlerContext<>).MakeGenericType(dc.ImplementationType);
+
+                return (IContext)container.GetInstance(contextType);
+            });
+
+            // IntCommandHandler depends on IContext.
+            container.Register<ICommandHandler<int>, IntCommandHandler>();
+
+            // Act
+            // CommandHandlerWrapper<T> depends on ICommandHandler<T>
+            var wrapper = container.GetInstance<CommandHandlerWrapper<int>>();
+
+            // Assert
+            Assert.IsInstanceOfType(wrapper.InjectedContext,
+                typeof(CommandHandlerContext<CommandHandlerWrapper<int>>));
+        }
+
+        private sealed class CommandHandlerWrapper<T>
+        {
+            public CommandHandlerWrapper(ICommandHandler<T> handler, IContext context, 
+                ConcreteCommand justAnExtraArgumentToMakeUsFindBugsFaster)
+            {
+                this.InjectedHandler = handler;
+                this.InjectedContext = context;
+            }
+
+            public ICommandHandler<T> InjectedHandler { get; private set; }
+
+            public IContext InjectedContext { get; private set; }
+        }
+
+        private sealed class IntCommandHandler : ICommandHandler<int>
+        {
+            public IntCommandHandler(IContext context)
+            {
+                this.InjectedContext = context;
+            }
+
+            public IContext InjectedContext { get; private set; }
+
+            public void Execute(int command)
+            {
+                // Not important.
+            }
+        }
+
+        private sealed class CommandHandlerContext<TCommandHandler> : IContext
+        {
+        }
+    }
+}
---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/ContextDependentExtensions.cs
@@ -0,0 +1,126 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    using SimpleInjector;
+
+    public class DependencyContext
+    {
+        internal static readonly DependencyContext Root = new DependencyContext();
+
+        internal DependencyContext(Type serviceType, Type implementationType)
+        {
+            this.ServiceType = serviceType;
+            this.ImplementationType = implementationType;
+        }
+
+        private DependencyContext()
+        {
+        }
+
+        /// <summary>Gets the service type in which the current type is created or null when there is 
+        /// no parent (will happen when the type is directly created using container.GetInstance).</summary>
+        /// <value>A <see cref="Type"/>.</value>
+        public Type ServiceType { get; private set; }
+
+        /// <summary>Gets the actual implementation in which the current type is created or null when there is
+        /// no parent (will happen when the type is directly created using container.GetInstance).</summary>
+        /// <value>A <see cref="Type"/>.</value>
+        public Type ImplementationType { get; private set; }
+    }
+
+    public static class ContextDependentExtensions
+    {
+        public static void RegisterWithContext<TService>(this Container container,
+            Func<DependencyContext, TService> contextBasedInstanceCreator)
+            where TService : class
+        {
+            AllowServiceToBeResolvedAsRootType<TService>(container, contextBasedInstanceCreator);
+
+            AllowServiceToBeResolvedAsDependency<TService>(container, contextBasedInstanceCreator);
+        }
+
+        private static void AllowServiceToBeResolvedAsRootType<TService>(Container container, 
+            Func<DependencyContext, TService> contextBasedInstanceCreator) where TService : class
+        {
+            // Allow TService to be resolved as root type (i.e. calls to container.GetInstance<TService>());
+            container.ResolveUnregisteredType += (sender, e) =>
+            {
+                if (e.UnregisteredServiceType == typeof(TService))
+                {
+                    Func<object> rootInstanceCreator = 
+                        () => contextBasedInstanceCreator(DependencyContext.Root);
+
+                    e.Register(rootInstanceCreator);
+                }
+            };
+        }
+
+        private static void AllowServiceToBeResolvedAsDependency<TService>(Container container, 
+            Func<DependencyContext, TService> contextBasedInstanceCreator) where TService : class
+        {
+            // Allow the Func<DependencyContext, TService> to be injected into transient parent types.
+            container.Intercepting += (sender, e) =>
+            {
+                bool serviceIsADirectDependency =
+                    ServiceIsADirectDependencyOfType(e.Expression, typeof(TService));
+
+                if (serviceIsADirectDependency)
+                {
+                    var expression = (NewExpression)e.Expression;
+
+                    var arguments = BuildNewListOfConstructorArguments(e, contextBasedInstanceCreator);
+
+                    e.Expression = Expression.New(expression.Constructor, arguments.ToArray());
+                }
+            };
+        }
+
+        private static bool ServiceIsADirectDependencyOfType(Expression expression, Type serviceType)
+        {
+            var newExpression = expression as NewExpression;
+
+            // Only constructor calls can be intercepted.
+            if (newExpression == null)
+            {
+                return false;
+            }
+
+            var parameters = newExpression.Constructor.GetParameters();
+
+            bool constructorContainsService = parameters.Any(p => p.ParameterType == serviceType);
+
+            return constructorContainsService;
+        }
+
+        private static IEnumerable<Expression> BuildNewListOfConstructorArguments<TService>(
+            InterceptingEventArgs e, Func<DependencyContext, TService> contextBasedInstanceCreator)
+        {
+            var expression = (NewExpression)e.Expression;
+
+            var context = new DependencyContext(e.RegisteredServiceType, expression.Type);
+            var contextExpression = Expression.Constant(context);
+
+            var parameters = expression.Constructor.GetParameters();
+
+            for (int index = 0; index < expression.Arguments.Count; index++)
+            {
+                if (parameters[index].ParameterType == typeof(TService))
+                {
+                    // Change the argument to an invocation of the instanceCreator with a context.
+                    yield return
+                        Expression.Invoke(Expression.Constant(contextBasedInstanceCreator), contextExpression);
+                }
+                else
+                {
+                    // The original argument
+                    yield return expression.Arguments[index];
+                }
+            }
+        }
+    }
+}
