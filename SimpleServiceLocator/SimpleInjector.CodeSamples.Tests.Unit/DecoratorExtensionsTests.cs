305adf30f620f1f07c69e27a944bc133eae401b4
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-12-09 07:28:41 -0800

    Nre DecoratorExtensions class added to the CodeSamples project.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/DecoratorExtensionsTests.cs
@@ -0,0 +1,204 @@
+﻿namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SimpleInjector.Extensions;
+
+    [TestClass]
+    public class DecoratorExtensionsTests
+    {
+        public interface ICommandHandler<TCommand>
+        {
+            void Handle(TCommand command);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedType_ReturnsTheDecorator()
+        {
+            // Arrange
+            var logger = new FakeLogger();
+
+            var container = new Container();
+
+            container.RegisterSingle<ILogger>(logger);
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.IsInstanceOfType(handler, typeof(Decorator1Handler<RealCommand>));
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedType_GetsHandledAsExpected()
+        {
+            // Arrange
+            var logger = new FakeLogger();
+
+            var container = new Container();
+
+            container.RegisterSingle<ILogger>(logger);
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+
+            // Act
+            container.GetInstance<ICommandHandler<RealCommand>>().Handle(new RealCommand());
+
+            // Assert
+            Assert.AreEqual("Begin1 RealCommand End1", logger.Message);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances_ReturnsLastRegisteredDecorator()
+        {
+            // Arrange
+            var logger = new FakeLogger();
+
+            var container = new Container();
+
+            container.RegisterSingle<ILogger>(logger);
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator2Handler<>));
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.IsInstanceOfType(handler, typeof(Decorator2Handler<RealCommand>));
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances_GetsHandledAsExpected()
+        {
+            // Arrange
+            var logger = new FakeLogger();
+
+            var container = new Container();
+
+            container.RegisterSingle<ILogger>(logger);
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator2Handler<>));
+
+            // Act
+            container.GetInstance<ICommandHandler<RealCommand>>().Handle(new RealCommand());
+
+            // Assert
+            Assert.AreEqual("Begin2 Begin1 RealCommand End1 End2", logger.Message);
+        }
+
+        [TestMethod]
+        public void GetInstance_DecoratorWithMissingDependency_ThrowAnExceptionWithADescriptiveMessage()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            // Decorator1Handler depends on ILogger, but ILogger is not registered.
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+
+            try
+            {
+                // Act
+                var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+                // Assert
+                Assert.Fail("Exception was expected.");
+            }
+            catch (ActivationException ex)
+            {
+                Assert.IsTrue(ex.Message.Contains("ILogger"), "Actual message: " + ex.Message);
+            }
+        }
+
+        private sealed class FakeLogger : ILogger
+        {
+            public string Message { get; private set; }
+
+            public void Log(string message)
+            {
+                this.Message += message;
+            }
+        }
+
+        private class RealCommand 
+        {
+        }
+
+        private class StubCommandHandler : ICommandHandler<RealCommand>
+        {
+            public void Handle(RealCommand command)
+            {
+            }
+        }
+
+        private class RealCommandHandler : ICommandHandler<RealCommand>
+        {
+            private readonly ILogger logger;
+
+            public RealCommandHandler(ILogger logger)
+            {
+                this.logger = logger;
+            }
+
+            public void Handle(RealCommand command)
+            {
+                this.logger.Log("RealCommand");
+            }
+        }
+
+        private class Decorator1Handler<T> : ICommandHandler<T>
+        {
+            private readonly ICommandHandler<T> wrapped;
+            private readonly ILogger logger;
+
+            public Decorator1Handler(ICommandHandler<T> wrapped, ILogger logger)
+            {
+                this.wrapped = wrapped;
+                this.logger = logger;
+            }
+
+            public void Handle(T command)
+            {
+                this.logger.Log("Begin1 ");
+                this.wrapped.Handle(command);
+                this.logger.Log(" End1");
+            }
+        }
+
+        private class Decorator2Handler<T> : ICommandHandler<T>
+        {
+            private readonly ICommandHandler<T> wrapped;
+            private readonly ILogger logger;
+
+            public Decorator2Handler(ICommandHandler<T> wrapped, ILogger logger)
+            {
+                this.wrapped = wrapped;
+                this.logger = logger;
+            }
+
+            public void Handle(T command)
+            {
+                this.logger.Log("Begin2 ");
+                this.wrapped.Handle(command);
+                this.logger.Log(" End2");
+            }
+        }
+    }
+}
---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/DecoratorExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public static class DecoratorExtensions
+    {
+        public static void RegisterOpenGenericDecorator(this Container container,
+            Type openGenericType, Type openGenericDecorator)
+        {
+            if (openGenericType.GetGenericArguments().Length != openGenericDecorator.GetGenericArguments().Length)
+            {
+                throw new ArgumentException("The generic arguments of openGenericDecorator and " + 
+                    "openGenericType do not match", "openGenericDecorator");
+            }
+
+            if (openGenericDecorator.GetConstructors().Length != 1)
+            {
+                throw new ArgumentException("The type should have exactly one public constructor.", 
+                    "openGenericDecorator");
+            }
+
+            if (openGenericDecorator.GetConstructors().Single().GetParameters()
+                .Any(p => p.ParameterType == openGenericType))
+            {
+                throw new ArgumentException(string.Format("The type is not a decorator, because {0} is not " +
+                    "a constructor argument.", openGenericType), "openGenericDecorator");
+            }
+
+            container.ExpressionBuilt += (sender, e) =>
+            {
+                var serviceType = e.RegisteredServiceType;
+
+                if (serviceType.IsGenericType && serviceType.GetGenericTypeDefinition() == openGenericType)
+                {
+                    var closedGenericDecorator =
+                        openGenericDecorator.MakeGenericType(serviceType.GetGenericArguments());
+
+                    var ctor = closedGenericDecorator.GetConstructors().Single();
+
+                    var arguments = 
+                        from parameter in ctor.GetParameters()
+                        let type = parameter.ParameterType
+                        select type == serviceType ? e.Expression : 
+                            container.GetRegistration(type, true).BuildExpression();
+
+                    e.Expression = Expression.New(ctor, arguments.ToArray());
+                }
+            };
+        }
+    }
+}
