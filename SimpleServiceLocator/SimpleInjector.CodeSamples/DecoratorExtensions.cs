9612ca9db9390eed16df1131ce9e7f17feef2e44
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-12-09 18:09:02 -0800

    DecoratorExtensions class in CodeSamples project cleaned up.

--- SimpleServiceLocator/SimpleInjector.CodeSamples/DecoratorExtensions.cs
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/DecoratorExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace SimpleInjector.CodeSamples
 {
+    // http://simpleinjector.codeplex.com/wikipage?title=DecoratorExtensions
     using System;
     using System.Linq;
     using System.Linq.Expressions;
@@ -9,25 +10,6 @@
         public static void RegisterOpenGenericDecorator(this Container container,
             Type openGenericType, Type openGenericDecorator)
         {
-            if (openGenericType.GetGenericArguments().Length != openGenericDecorator.GetGenericArguments().Length)
-            {
-                throw new ArgumentException("The generic arguments of openGenericDecorator and " + 
-                    "openGenericType do not match", "openGenericDecorator");
-            }
-
-            if (openGenericDecorator.GetConstructors().Length != 1)
-            {
-                throw new ArgumentException("The type should have exactly one public constructor.", 
-                    "openGenericDecorator");
-            }
-
-            if (openGenericDecorator.GetConstructors().Single().GetParameters()
-                .Any(p => p.ParameterType == openGenericType))
-            {
-                throw new ArgumentException(string.Format("The type is not a decorator, because {0} is not " +
-                    "a constructor argument.", openGenericType), "openGenericDecorator");
-            }
-
             container.ExpressionBuilt += (sender, e) =>
             {
                 var serviceType = e.RegisteredServiceType;
@@ -39,13 +21,11 @@
 
                     var ctor = closedGenericDecorator.GetConstructors().Single();
 
-                    var arguments = 
+                    e.Expression = Expression.New(ctor,
                         from parameter in ctor.GetParameters()
                         let type = parameter.ParameterType
-                        select type == serviceType ? e.Expression : 
-                            container.GetRegistration(type, true).BuildExpression();
-
-                    e.Expression = Expression.New(ctor, arguments.ToArray());
+                        select type == serviceType ? e.Expression :
+                            container.GetRegistration(type, true).BuildExpression());
                 }
             };
         }
