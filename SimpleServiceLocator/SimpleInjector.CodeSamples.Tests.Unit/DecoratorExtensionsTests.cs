e6bc5a83f9931581a4e92b29cb43712a3fb3c546
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2011-12-10 05:27:54 -0800

    DecoratorExtensions in the CodeSamples updated. It now allows supplying a predicate to determine whether the decorator must be applied.

--- SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/DecoratorExtensionsTests.cs
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/DecoratorExtensionsTests.cs
@@ -1,9 +1,8 @@
 ﻿namespace SimpleInjector.CodeSamples.Tests.Unit
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+    using System.Linq.Expressions;
+
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     using SimpleInjector.Extensions;
@@ -28,13 +27,13 @@
 
             container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
 
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator1<>));
 
             // Act
             var handler = container.GetInstance<ICommandHandler<RealCommand>>();
 
             // Assert
-            Assert.IsInstanceOfType(handler, typeof(Decorator1Handler<RealCommand>));
+            Assert.IsInstanceOfType(handler, typeof(HandlerDecorator1<RealCommand>));
         }
 
         [TestMethod]
@@ -49,7 +48,7 @@
 
             container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
 
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator1<>));
 
             // Act
             container.GetInstance<ICommandHandler<RealCommand>>().Handle(new RealCommand());
@@ -70,14 +69,14 @@
 
             container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
 
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator2Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator1<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator2<>));
 
             // Act
             var handler = container.GetInstance<ICommandHandler<RealCommand>>();
 
             // Assert
-            Assert.IsInstanceOfType(handler, typeof(Decorator2Handler<RealCommand>));
+            Assert.IsInstanceOfType(handler, typeof(HandlerDecorator2<RealCommand>));
         }
 
         [TestMethod]
@@ -92,8 +91,8 @@
 
             container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(RealCommandHandler));
 
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator2Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator1<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator2<>));
 
             // Act
             container.GetInstance<ICommandHandler<RealCommand>>().Handle(new RealCommand());
@@ -111,7 +110,7 @@
             container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
 
             // Decorator1Handler depends on ILogger, but ILogger is not registered.
-            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(Decorator1Handler<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(HandlerDecorator1<>));
 
             try
             {
@@ -125,6 +124,322 @@
             {
                 Assert.IsTrue(ex.Message.Contains("ILogger"), "Actual message: " + ex.Message);
             }
+        }
+
+        [TestMethod]
+        public void GetInstance_DecoratorPredicateReturnsFalse_DoesNotDecorateInstance()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), 
+                c => false);
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.IsInstanceOfType(handler, typeof(StubCommandHandler));
+        }
+
+        [TestMethod]
+        public void GetInstance_DecoratorPredicateReturnsTrue_DecoratesInstance()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>),
+                c => true);
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.IsInstanceOfType(handler, typeof(StubDecorator1<RealCommand>));
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedType_CallsThePredicateWithTheExpectedServiceType()
+        {
+            // Arrange
+            Type expectedPredicateServiceType = typeof(ICommandHandler<RealCommand>);
+            Type actualPredicateServiceType = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateServiceType = c.ServiceType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateServiceType, actualPredicateServiceType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedTransient_CallsThePredicateWithTheExpectedImplementationType()
+        {
+            // Arrange
+            Type expectedPredicateImplementationType = typeof(StubCommandHandler);
+            Type actualPredicateImplementationType = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateImplementationType = c.ImplementationType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateImplementationType, actualPredicateImplementationType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedTransientWithInitializer_CallsThePredicateWithTheExpectedImplementationType()
+        {
+            // Arrange
+            Type expectedPredicateImplementationType = typeof(StubCommandHandler);
+            Type actualPredicateImplementationType = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterInitializer<StubCommandHandler>(handlerToInitialize => { });
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateImplementationType = c.ImplementationType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateImplementationType, actualPredicateImplementationType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedSingleton_CallsThePredicateWithTheExpectedImplementationType()
+        {
+            // Arrange
+            Type expectedPredicateImplementationType = typeof(StubCommandHandler);
+            Type actualPredicateImplementationType = null;
+
+            var container = new Container();
+
+            container.RegisterSingle<ICommandHandler<RealCommand>>(new StubCommandHandler());
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateImplementationType = c.ImplementationType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateImplementationType, actualPredicateImplementationType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedTypeRegisteredWithFuncDelegate_CallsThePredicateWithTheImplementationTypeEqualsServiceType()
+        {
+            // Arrange
+            Type expectedPredicateImplementationType = typeof(ICommandHandler<RealCommand>);
+            Type actualPredicateImplementationType = null;
+
+            var container = new Container();
+
+            // Because we registere a Func<TServiceType> there is no way we can determine the implementation 
+            // type. In that case the ImplementationType should equal the ServiceType.
+            container.Register<ICommandHandler<RealCommand>>(() => new StubCommandHandler());
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateImplementationType = c.ImplementationType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateImplementationType, actualPredicateImplementationType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances_CallsThePredicateWithTheExpectedImplementationType()
+        {
+            // Arrange
+            Type expectedPredicateImplementationType = typeof(StubCommandHandler);
+            Type actualPredicateImplementationType = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>));
+            
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>), c =>
+            {
+                actualPredicateImplementationType = c.ImplementationType;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(expectedPredicateImplementationType, actualPredicateImplementationType);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedType_CallsThePredicateWithAnExpression()
+        {
+            // Arrange
+            Expression actualPredicateExpression = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                actualPredicateExpression = c.Expression;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.IsNotNull(actualPredicateExpression);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances_SuppliesADifferentExpressionToTheSecondPredicate()
+        {
+            // Arrange
+            Expression predicateExpressionOnFirstCall = null;
+            Expression predicateExpressionOnSecondCall = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>), c =>
+            {
+                predicateExpressionOnFirstCall = c.Expression;
+                return true;
+            });
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>), c =>
+            {
+                predicateExpressionOnSecondCall = c.Expression;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreNotEqual(predicateExpressionOnFirstCall, predicateExpressionOnSecondCall,
+                "The predicate was expected to change, because the first decorator has been applied.");
+        }
+
+        [TestMethod]
+        public void GetInstance_OnDecoratedType_SuppliesNoAppliedDecoratorsToThePredicate()
+        {
+            // Arrange
+            Type[] appliedDecorators = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>), c =>
+            {
+                appliedDecorators = c.AppliedDecorators;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(0, appliedDecorators.Length);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances1_SuppliesNoAppliedDecoratorsToThePredicate()
+        {
+            // Arrange
+            Type[] appliedDecorators = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>), c =>
+            {
+                appliedDecorators = c.AppliedDecorators;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(1, appliedDecorators.Length);
+            Assert.AreEqual(typeof(StubDecorator1<RealCommand>), appliedDecorators[0]);
+        }
+
+        [TestMethod]
+        public void GetInstance_OnTypeDecoratedByMultipleInstances2_SuppliesNoAppliedDecoratorsToThePredicate()
+        {
+            // Arrange
+            Type[] appliedDecorators = null;
+
+            var container = new Container();
+
+            container.RegisterManyForOpenGeneric(typeof(ICommandHandler<>), typeof(StubCommandHandler));
+
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator1<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>));
+            container.RegisterOpenGenericDecorator(typeof(ICommandHandler<>), typeof(StubDecorator2<>), c =>
+            {
+                appliedDecorators = c.AppliedDecorators;
+                return true;
+            });
+
+            // Act
+            var handler = container.GetInstance<ICommandHandler<RealCommand>>();
+
+            // Assert
+            Assert.AreEqual(2, appliedDecorators.Length);
+            Assert.AreEqual(typeof(StubDecorator1<RealCommand>), appliedDecorators[0]);
+            Assert.AreEqual(typeof(StubDecorator2<RealCommand>), appliedDecorators[1]);
         }
 
         private sealed class FakeLogger : ILogger
@@ -163,12 +478,34 @@
             }
         }
 
-        private class Decorator1Handler<T> : ICommandHandler<T>
+        private class StubDecorator1<T> : ICommandHandler<T>
+        {
+            public StubDecorator1(ICommandHandler<T> wrapped)
+            {
+            }
+
+            public void Handle(T command)
+            {
+            }
+        }
+
+        private class StubDecorator2<T> : ICommandHandler<T>
+        {
+            public StubDecorator2(ICommandHandler<T> wrapped)
+            {
+            }
+
+            public void Handle(T command)
+            {
+            }
+        }
+
+        private class HandlerDecorator1<T> : ICommandHandler<T>
         {
             private readonly ICommandHandler<T> wrapped;
             private readonly ILogger logger;
 
-            public Decorator1Handler(ICommandHandler<T> wrapped, ILogger logger)
+            public HandlerDecorator1(ICommandHandler<T> wrapped, ILogger logger)
             {
                 this.wrapped = wrapped;
                 this.logger = logger;
@@ -182,12 +519,12 @@
             }
         }
 
-        private class Decorator2Handler<T> : ICommandHandler<T>
+        private class HandlerDecorator2<T> : ICommandHandler<T>
         {
             private readonly ICommandHandler<T> wrapped;
             private readonly ILogger logger;
 
-            public Decorator2Handler(ICommandHandler<T> wrapped, ILogger logger)
+            public HandlerDecorator2(ICommandHandler<T> wrapped, ILogger logger)
             {
                 this.wrapped = wrapped;
                 this.logger = logger;
--- SimpleServiceLocator/SimpleInjector.CodeSamples/DecoratorExtensions.cs
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/DecoratorExtensions.cs
@@ -2,22 +2,120 @@
 {
     // http://simpleinjector.codeplex.com/wikipage?title=DecoratorExtensions
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
 
+    public class DecoratorPredicateContext
+    {
+        public Type ServiceType { get; set; }
+
+        public Type ImplementationType { get; set; }
+
+        public Type[] AppliedDecorators { get; set; }
+
+        public Expression Expression { get; set; }
+    }
+
     public static class DecoratorExtensions
     {
+        [ThreadStatic]
+        private static Dictionary<Type, ServiceTypeDecoratorInfo> serviceTypeInfos;
+
         public static void RegisterOpenGenericDecorator(this Container container,
             Type openGenericType, Type openGenericDecorator)
         {
-            container.ExpressionBuilt += (sender, e) =>
+            container.RegisterOpenGenericDecorator(openGenericType, openGenericDecorator, c => true);
+        }
+
+        public static void RegisterOpenGenericDecorator(this Container container,
+            Type openGenericType, Type openGenericDecorator, Func<DecoratorPredicateContext, bool> predicate)
+        {
+            var helper = new DecorationHelper
+            {
+                Container = container,
+                OpenGenericType = openGenericType,
+                OpenGenericDecorator = openGenericDecorator,
+                Predicate = predicate
+            };
+
+            container.ExpressionBuilt += helper.OnExpressionBuild;
+        }
+
+        private static DecoratorPredicateContext CreatePredicateContext(ExpressionBuiltEventArgs e)
+        {
+            var info = GetServiceTypeInfo(e);
+
+            return new DecoratorPredicateContext
+            {
+                ServiceType = e.RegisteredServiceType,
+                Expression = e.Expression,
+                ImplementationType = info.ImplementationType,
+                AppliedDecorators = info.AppliedDecorators.ToArray()
+            };
+        }
+
+        private static ServiceTypeDecoratorInfo GetServiceTypeInfo(ExpressionBuiltEventArgs e)
+        {
+            var cache =
+                serviceTypeInfos ?? (serviceTypeInfos = new Dictionary<Type, ServiceTypeDecoratorInfo>());
+
+            if (!cache.ContainsKey(e.RegisteredServiceType))
+            {
+                cache[e.RegisteredServiceType] =
+                    new ServiceTypeDecoratorInfo(DetermineImplementationType(e));
+            }
+
+            return cache[e.RegisteredServiceType];
+        }
+
+        private static Type DetermineImplementationType(ExpressionBuiltEventArgs e)
+        {
+            if (e.Expression is ConstantExpression)
+            {
+                // Singleton
+                return ((ConstantExpression)e.Expression).Value.GetType();
+            }
+
+            if (e.Expression is NewExpression)
+            {
+                // Transient without initializers.
+                return ((NewExpression)e.Expression).Constructor.DeclaringType;
+            }
+
+            var invocation = e.Expression as InvocationExpression;
+
+            if (invocation != null && invocation.Expression is ConstantExpression &&
+                invocation.Arguments.Count == 1 && invocation.Arguments[0] is NewExpression)
+            {
+                // Transient with initializers.
+                return ((NewExpression)invocation.Arguments[0]).Constructor.DeclaringType;
+            }
+
+            // Implementation type can not be determined.
+            return e.RegisteredServiceType;
+        }
+
+        private sealed class DecorationHelper
+        {
+            public Container Container { get; set; }
+
+            public Type OpenGenericType { get; set; }
+
+            public Type OpenGenericDecorator { get; set; }
+
+            public Func<DecoratorPredicateContext, bool> Predicate { get; set; }
+
+            public void OnExpressionBuild(object sender, ExpressionBuiltEventArgs e)
             {
                 var serviceType = e.RegisteredServiceType;
 
-                if (serviceType.IsGenericType && serviceType.GetGenericTypeDefinition() == openGenericType)
+                if (serviceType.IsGenericType && 
+                    serviceType.GetGenericTypeDefinition() == this.OpenGenericType &&
+                    this.Predicate(CreatePredicateContext(e)))
                 {
                     var closedGenericDecorator =
-                        openGenericDecorator.MakeGenericType(serviceType.GetGenericArguments());
+                        this.OpenGenericDecorator.MakeGenericType(serviceType.GetGenericArguments());
 
                     var ctor = closedGenericDecorator.GetConstructors().Single();
 
@@ -25,9 +123,24 @@
                         from parameter in ctor.GetParameters()
                         let type = parameter.ParameterType
                         select type == serviceType ? e.Expression :
-                            container.GetRegistration(type, true).BuildExpression());
+                            this.Container.GetRegistration(type, true).BuildExpression());
+
+                    GetServiceTypeInfo(e).AppliedDecorators.Add(closedGenericDecorator);
                 }
-            };
+            }
+        }
+
+        private sealed class ServiceTypeDecoratorInfo
+        {
+            public ServiceTypeDecoratorInfo(Type implementationType)
+            {
+                this.ImplementationType = implementationType;
+                this.AppliedDecorators = new List<Type>();
+            }
+
+            public Type ImplementationType { get; private set; }
+
+            public List<Type> AppliedDecorators { get; private set; }
         }
     }
 }
