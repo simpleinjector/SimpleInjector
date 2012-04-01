83de2147ea001cf64f8c4d78d6bcc919a018005d
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2012-04-01 03:34:54 -0700

    New RegisterPerWebRequest<TConcrete>(this Container container) overload added to the PerWebRequest examples.

--- SimpleServiceLocator/SimpleInjector.CodeSamples/PerWebRequestRegistrationsExtensions.cs
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/PerWebRequestRegistrationsExtensions.cs
@@ -3,6 +3,7 @@
     // http://simpleinjector.codeplex.com/wikipage?title=PerWebRequestExtensionMethod
     using System;
     using System.Diagnostics;
+    using System.Linq.Expressions;
     using System.Web;
     using SimpleInjector;
 
@@ -11,26 +12,59 @@
     /// </summary>
     public static partial class SimpleInjectorPerWebRequestExtensions
     {
+        [DebuggerStepThrough]
         public static void RegisterPerWebRequest<TService, TImplementation>(
             this Container container)
             where TService : class
             where TImplementation : class, TService
         {
-            Func<TService> instanceCreator = 
+            Func<TService> instanceCreator =
                 () => container.GetInstance<TImplementation>();
 
             container.RegisterPerWebRequest<TService>(instanceCreator);
         }
 
+        [DebuggerStepThrough]
         public static void RegisterPerWebRequest<TService>(this Container container,
             Func<TService> instanceCreator) where TService : class
         {
-            var creator = 
+            var creator =
                 new PerWebRequestInstanceCreator<TService>(instanceCreator);
 
             container.Register<TService>(creator.GetInstance);
         }
 
+        [DebuggerStepThrough]
+        public static void RegisterPerWebRequest<TConcrete>(this Container container)
+            where TConcrete : class
+        {
+            // Register the type as transient. This prevents it from being registered 
+            // twice and allows us to hook onto the ExpressionBuilt event.
+            container.Register<TConcrete>();
+
+            container.ExpressionBuilt += (sender, e) =>
+            {
+                // e.Expression contains an new TConcrete(...) call where that type is
+                // auto-wired by the container. There is no other way than using
+                // ExpressionBuilt to get this auto-wired instance.
+                if (e.RegisteredServiceType == typeof(TConcrete))
+                {
+                    // Extract a Func<T> delegate for creating the transient TConcrete.
+                    var transientInstanceCreator = Expression.Lambda<Func<TConcrete>>(
+                        e.Expression, new ParameterExpression[0]).Compile();
+
+                    var creator = new PerWebRequestInstanceCreator<TConcrete>(
+                        transientInstanceCreator);
+
+                    // Swap the original expression so that the lifetime becomes per
+                    // web request.
+                    e.Expression = Expression.Call(Expression.Constant(creator),
+                        creator.GetType().GetMethod("GetInstance"));
+                }
+            };
+        }
+
+        [DebuggerStepThrough]
         public static void DisposeInstance<TService>() where TService : class
         {
             object key = typeof(PerWebRequestInstanceCreator<TService>);
@@ -53,7 +87,7 @@
             }
 
             [DebuggerStepThrough]
-            internal T GetInstance()
+            public T GetInstance()
             {
                 var context = HttpContext.Current;
 
