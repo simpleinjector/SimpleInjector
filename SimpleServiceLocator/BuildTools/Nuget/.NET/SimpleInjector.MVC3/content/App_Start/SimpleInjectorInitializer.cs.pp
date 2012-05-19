4def15d5481b99deee48d5e469b7232a3d37cb7e
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2012-05-19 04:18:24 -0700

    BuildTools folder added to solution, which contains tools for the automated build process that will run when a new version gets released.

---
+++ SimpleServiceLocator/BuildTools/Nuget/.NET/SimpleInjector.MVC3/content/App_Start/SimpleInjectorInitializer.cs.pp
@@ -0,0 +1,37 @@
+[assembly: WebActivator.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.SimpleInjectorInitializer), "Initialize")]
+
+namespace $rootnamespace$.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web.Mvc;
+    
+    public static class SimpleInjectorInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            var container = new Container();
+            
+            InitializeContainer(container);
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            
+            container.RegisterMvcAttributeFilterProvider();
+       
+            container.Verify();
+            
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+#error Register your services here (remove this line).
+
+            // For instance:
+            // container.Register<IUserRepository, SqlUserRepository>();
+        }
+    }
+}
