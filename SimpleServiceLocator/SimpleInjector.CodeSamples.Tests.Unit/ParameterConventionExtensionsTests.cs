72a78391afab1d89fb55553b5be4b9f9b69d2643
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2012-07-09 13:39:41 -0700

    ParameterConventionExtensions class added to the CodeSamples project to show case the new constructor behavior extension point. This extension method allows convention based injection of primitive constructor arguments.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/ParameterConventionExtensionsTests.cs
@@ -0,0 +1,374 @@
+﻿namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    using System;
+    using System.Configuration;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ParameterConventionExtensionsTests
+    {
+        [TestMethod]
+        public void ConnectionStringsConvention_RegisteringTypeWithConnectionStringParameter_Succeeds()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            // Act
+            container.Register<TypeWithConnectionStringConstructorArgument>();
+        }
+
+        [TestMethod]
+        public void ConnectionStringsConvention_ResolvingTypeWithConnectionStringParameter_InjectsExpectedValue()
+        {
+            // Arrange
+            string expectedConnectionString = ConfigurationManager.ConnectionStrings["cs1"].ConnectionString;
+
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            // Act
+            var instance = container.GetInstance<TypeWithConnectionStringConstructorArgument>();
+
+            // Assert
+            Assert.AreEqual(expectedConnectionString, instance.ConnectionString);
+        }
+
+        [TestMethod]
+        public void ConnectionStringsConvention_RegisteringTypeWithIntParameterWhichNameEndsWithConnectionString_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            try
+            {
+                // Act
+                container.Register<TypeWithConnectionStringIntConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception expected.");
+            }
+            catch (ArgumentException ex)
+            {
+                AssertThat.StringContains(
+                    "The constructor of type TypeWithConnectionStringIntConstructorArgument contains " +
+                    "parameter 'cs1ConnectionString' of type Int32 which can not be used for constructor " +
+                    "injection because it is a value type.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ConnectionStringsConvention_ResolvingTypeWithIntParameterWhichNameEndsWithConnectionString_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            try
+            {
+                // Act
+                container.GetInstance<TypeWithConnectionStringIntConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception expected.");
+            }
+            catch (ActivationException ex)
+            {
+                AssertThat.StringContains(
+                    "The constructor of type TypeWithConnectionStringIntConstructorArgument contains " +
+                    "parameter 'cs1ConnectionString' of type Int32 which can not be used for constructor " +
+                    "injection because it is a value type.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ConnectionStringsConvention_RegisteringTypeWithNonExistingConnectionStringParameter_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            try
+            {
+                // Act
+                container.Register<TypeWithNonExistingConnectingStringConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception was expected.");
+            }
+            catch (ArgumentException ex)
+            {
+                AssertThat.StringContains(
+                    "No connection string with name 'doesNotExist' could be found", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ConnectionStringsConvention_ResolvingTypeWithNonExistingConnectionStringParameter_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new ConnectionStringsConvention());
+
+            try
+            {
+                // Act
+                container.GetInstance<TypeWithNonExistingConnectingStringConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception was expected.");
+            }
+            catch (ActivationException ex)
+            {
+                AssertThat.StringContains(
+                    "No connection string with name 'doesNotExist' could be found", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_RegisteringTypeWithStringAppSettingParameter_Succeeds()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            // Act
+            container.Register<TypeWithAppSettingsStringConstructorArgument>();
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_ResolvingTypeWithStringAppSettingParameter_InjectsExpectedValue()
+        {
+            // Arrange
+            string expectedAppSetting = ConfigurationManager.AppSettings["as1"];
+
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            // Act
+            var instance = container.GetInstance<TypeWithAppSettingsStringConstructorArgument>();
+
+            // Assert
+            Assert.AreEqual(expectedAppSetting, instance.AppSetting);
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_RegisteringTypeWithGuidAppSettingParameter_Succeeds()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            // Act
+            container.Register<TypeWithAppSettingsStringConstructorArgument>();
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_ResolvingTypeWithGuidAppSettingParameter_InjectsExpectedValue()
+        {
+            // Arrange
+            Guid expectedAppSetting = Guid.Parse(ConfigurationManager.AppSettings["as2"]);
+
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            // Act
+            var instance = container.GetInstance<TypeWithAppSettingsGuidConstructorArgument>();
+
+            // Assert
+            Assert.AreEqual(expectedAppSetting, instance.AppSetting);
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_RegisteringTypeWithReferenceTypeParameterWhichNameEndsWithAppSetting_Succeeds()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            // Act
+            // This type depends on an reference type and since reference types are not handled by the
+            // AppSettingsConvention, it should fallback on the default behavior, and since reference types
+            // can be resolved by the container, the registration is therefore valid.
+            container.Register<TypeWithAppSettingConstructorArgumentOfReferenceType>();
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_ResolvingTypeWithReferenceTypeParameterWhichNameEndsWithConnectionString_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            try
+            {
+                // Act
+                container.GetInstance<TypeWithAppSettingConstructorArgumentOfReferenceType>();
+
+                // Assert
+                Assert.Fail("Exception expected.");
+            }
+            catch (ActivationException ex)
+            {
+                AssertThat.StringContains(
+                    "The constructor of the type TypeWithAppSettingConstructorArgumentOfReferenceType " +
+                    "contains the parameter of type IDisposable with name 'as1AppSetting' that is not " +
+                    "registered.", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_RegisteringTypeWithNonExistingAppSettingsParameter_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            try
+            {
+                // Act
+                container.Register<TypeWithNonExistingAppSettingConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception was expected.");
+            }
+            catch (ArgumentException ex)
+            {
+                AssertThat.StringContains(
+                    "No app setting with key 'doesNotExist' could be found", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void AppSettingsConvention_ResolvingTypeWithNonExistingConnectionStringParameter_FailsWithExpectedException()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(new AppSettingsConvention());
+
+            try
+            {
+                // Act
+                container.GetInstance<TypeWithNonExistingAppSettingConstructorArgument>();
+
+                // Assert
+                Assert.Fail("Exception was expected.");
+            }
+            catch (ActivationException ex)
+            {
+                AssertThat.StringContains(
+                    "No app setting with key 'doesNotExist' could be found", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void MultipleParameterConventions_RegisterATypeWithMultipleConventionBasedParameters_Succeeds()
+        {
+            // Arrange
+            var container = CreateContainerWithConvention(
+                new ConnectionStringsConvention(),
+                new AppSettingsConvention());
+
+            // Act
+            container.Register<TypeWithBothConnectionStringAndAppSettingsConstructorArguments>();
+        }
+
+        [TestMethod]
+        public void MultipleParameterConventions_ResolveATypeWithMultipleConventionBasedParameters_InjectsExpectedParameters()
+        {
+            // Arrange
+            string expectedConnectionString = ConfigurationManager.ConnectionStrings["cs1"].ConnectionString;
+            string expectedAppSetting = ConfigurationManager.AppSettings["as2"];
+
+            var container = CreateContainerWithConvention(
+                new ConnectionStringsConvention(),
+                new AppSettingsConvention());
+
+            // Act
+            var instance =
+                container.GetInstance<TypeWithBothConnectionStringAndAppSettingsConstructorArguments>();
+
+            // Assert
+            Assert.AreEqual(expectedConnectionString, instance.ConnectionString);
+            Assert.AreEqual(expectedAppSetting, instance.AppSetting);
+        }
+
+        private static Container CreateContainerWithConvention(params IParameterConvention[] conventions)
+        {
+            var options = new ContainerOptions();
+
+            var container = new Container(options);
+
+            foreach (var convention in conventions)
+            {
+                options.AddParameterConvention(convention);
+            }
+
+            return container;
+        }
+
+        public class TypeWithConnectionStringConstructorArgument
+        {
+            // "cs1" is a connection string in the app.config of this test project.
+            public TypeWithConnectionStringConstructorArgument(string cs1ConnectionString)
+            {
+                this.ConnectionString = cs1ConnectionString;
+            }
+
+            public string ConnectionString { get; private set; }
+        }
+
+        public class TypeWithConnectionStringIntConstructorArgument
+        {
+            public TypeWithConnectionStringIntConstructorArgument(int cs1ConnectionString)
+            {
+            }
+        }
+
+        public class TypeWithNonExistingConnectingStringConstructorArgument
+        {
+            public TypeWithNonExistingConnectingStringConstructorArgument(string doesNotExistConnectionString)
+            {
+            }
+        }
+
+        public class TypeWithAppSettingsStringConstructorArgument
+        {
+            // "as1" is a app setting  in the app.config of this test project.
+            public TypeWithAppSettingsStringConstructorArgument(string as1AppSetting)
+            {
+                this.AppSetting = as1AppSetting;
+            }
+
+            public string AppSetting { get; private set; }
+        }
+
+        public class TypeWithAppSettingsGuidConstructorArgument
+        {
+            // "as2" is a app setting in the app.config of this test project.
+            public TypeWithAppSettingsGuidConstructorArgument(Guid as2AppSetting)
+            {
+                this.AppSetting = as2AppSetting;
+            }
+
+            public Guid AppSetting { get; private set; }
+        }
+
+        public class TypeWithAppSettingConstructorArgumentOfReferenceType
+        {
+            public TypeWithAppSettingConstructorArgumentOfReferenceType(IDisposable as1AppSetting)
+            {
+            }
+        }
+
+        public class TypeWithNonExistingAppSettingConstructorArgument
+        {
+            public TypeWithNonExistingAppSettingConstructorArgument(string doesNotExistAppSetting)
+            {
+            }
+        }
+
+        public class TypeWithBothConnectionStringAndAppSettingsConstructorArguments
+        {
+            // "cs1" is a connection string in the app.config of this test project.
+            // "as2" is a app setting in the app.config of this test project.
+            public TypeWithBothConnectionStringAndAppSettingsConstructorArguments(string cs1ConnectionString,
+                string as2AppSetting)
+            {
+                this.ConnectionString = cs1ConnectionString;
+                this.AppSetting = as2AppSetting;
+            }
+
+            public string ConnectionString { get; private set; }
+
+            public string AppSetting { get; private set; }
+        }
+    }
+}
---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/ParameterConventionExtensions.cs
@@ -0,0 +1,196 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.ComponentModel;
+    using System.Configuration;
+    using System.Globalization;
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    using SimpleInjector.Advanced;
+
+    public interface IParameterConvention
+    {
+        bool CanResolve(ParameterInfo parameter);
+
+        Expression BuildExpression(ParameterInfo parameter);
+    }
+
+    public static class ParameterConventionExtensions
+    {
+        public static void AddParameterConvention(
+            this ContainerOptions options,
+            IParameterConvention convention)
+        {
+            options.ConstructorVerificationBehavior =
+                new ConventionConstructorVerificationBehavior(
+                    options.ConstructorVerificationBehavior,
+                    convention);
+
+            options.ConstructorInjectionBehavior =
+                new ConventionConstructorInjectionBehavior(
+                    options.ConstructorInjectionBehavior,
+                    convention);
+        }
+
+        private class ConventionConstructorVerificationBehavior
+            : IConstructorVerificationBehavior
+        {
+            private IConstructorVerificationBehavior decorated;
+            private IParameterConvention convention;
+
+            public ConventionConstructorVerificationBehavior(
+                IConstructorVerificationBehavior decorated,
+                IParameterConvention convention)
+            {
+                this.decorated = decorated;
+                this.convention = convention;
+            }
+
+            public void Verify(ParameterInfo parameter)
+            {
+                if (!this.convention.CanResolve(parameter))
+                {
+                    this.decorated.Verify(parameter);
+                }
+            }
+        }
+
+        private class ConventionConstructorInjectionBehavior
+            : IConstructorInjectionBehavior
+        {
+            private IConstructorInjectionBehavior decorated;
+            private IParameterConvention convention;
+
+            public ConventionConstructorInjectionBehavior(
+                IConstructorInjectionBehavior decorated,
+                IParameterConvention convention)
+            {
+                this.decorated = decorated;
+                this.convention = convention;
+            }
+
+            public Expression BuildParameterExpression(
+                ParameterInfo parameter)
+            {
+                if (!this.convention.CanResolve(parameter))
+                {
+                    return this.decorated
+                        .BuildParameterExpression(parameter);
+                }
+
+                return this.convention.BuildExpression(parameter);
+            }
+        }
+    }
+
+    public class ConnectionStringsConvention : IParameterConvention
+    {
+        private const string ConnectionStringPostFix =
+            "ConnectionString";
+
+        public bool CanResolve(ParameterInfo parameter)
+        {
+            bool resolvable =
+                parameter.ParameterType == typeof(string) &&
+                parameter.Name.EndsWith(ConnectionStringPostFix) &&
+                parameter.Name.LastIndexOf(ConnectionStringPostFix) > 0;
+
+            if (resolvable)
+            {
+                this.VerifyConfigurationFile(parameter);
+            }
+
+            return resolvable;
+        }
+
+        public Expression BuildExpression(ParameterInfo parameter)
+        {
+            var constr = this.GetConnectionString(parameter);
+
+            return Expression.Constant(constr, typeof(string));
+        }
+
+        private void VerifyConfigurationFile(ParameterInfo parameter)
+        {
+            this.GetConnectionString(parameter);
+        }
+
+        private string GetConnectionString(ParameterInfo parameter)
+        {
+            string name = parameter.Name.Substring(0,
+                parameter.Name.LastIndexOf(ConnectionStringPostFix));
+
+            ConnectionStringSettings settings =
+                ConfigurationManager.ConnectionStrings[name];
+
+            if (settings == null)
+            {
+                throw new ActivationException(
+                    "No connection string with name '" + name +
+                    "' could be found in the application's " +
+                    "configuration file.");
+            }
+
+            return settings.ConnectionString;
+        }
+    }
+
+    public class AppSettingsConvention : IParameterConvention
+    {
+        private const string AppSettingsPostFix = "AppSetting";
+
+        public bool CanResolve(ParameterInfo parameter)
+        {
+            Type type = parameter.ParameterType;
+
+            bool resolvable =
+                (type.IsValueType || type == typeof(string)) &&
+                parameter.Name.EndsWith(AppSettingsPostFix) &&
+                parameter.Name.LastIndexOf(AppSettingsPostFix) > 0;
+
+            if (resolvable)
+            {
+                this.VerifyConfigurationFile(parameter);
+            }
+
+            return resolvable;
+        }
+
+        public Expression BuildExpression(ParameterInfo parameter)
+        {
+            object valueToInject = this.GetAppSettingValue(parameter);
+
+            return Expression.Constant(valueToInject,
+                parameter.ParameterType);
+        }
+
+        private void VerifyConfigurationFile(ParameterInfo parameter)
+        {
+            this.GetAppSettingValue(parameter);
+        }
+
+        private object GetAppSettingValue(ParameterInfo parameter)
+        {
+            string key = parameter.Name.Substring(0,
+                parameter.Name.LastIndexOf(AppSettingsPostFix));
+
+            string configurationValue =
+                ConfigurationManager.AppSettings[key];
+
+            if (configurationValue == null)
+            {
+                throw new ActivationException(
+                    "No app setting with key '" + key + "' " +
+                    "could be found in the application's " +
+                    "configuration file.");
+            }
+
+            TypeConverter converter = TypeDescriptor.GetConverter(
+                parameter.ParameterType);
+
+            return converter.ConvertFromString(null,
+                CultureInfo.InvariantCulture, configurationValue);
+        }
+    }
+}
