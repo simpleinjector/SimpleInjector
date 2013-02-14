850f1fe078d68465ce9c1807c222094befb1a4ec
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2013-02-14 14:21:47 -0800

    AddRegistrationsTests class added.

---
+++ SimpleServiceLocator/SimpleInjector.NET.Tests.Unit/AddRegistrationTests.cs
@@ -0,0 +1,39 @@
+﻿namespace SimpleInjector.Tests.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddRegistrationTests
+    {
+        public interface IService1 
+        {
+        }
+        
+        public interface IService2 
+        {
+        }
+
+        [TestMethod]
+        public void GetInstanceOnTwoKeys_SameSingletonRegistrationForTwoKeys_ReturnsThatSameInstance()
+        {
+            // Arrange
+            var container = new Container();
+
+            var registration = Lifestyle.Singleton.CreateRegistration<Implementation, Implementation>(container);
+
+            container.AddRegistration(typeof(IService1), registration);
+            container.AddRegistration(typeof(IService2), registration);
+
+            // Act
+            var instance1 = container.GetInstance<IService1>();
+            var instance2 = container.GetInstance<IService2>();
+
+            // Assert
+            Assert.IsTrue(object.ReferenceEquals(instance1, instance2));
+        }
+
+        public class Implementation : IService1, IService2 
+        { 
+        }
+    }
+}
