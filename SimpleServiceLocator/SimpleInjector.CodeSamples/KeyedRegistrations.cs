dc2989aa2f7c8193f16a6b842379c3a2ef5b081c
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2013-05-28 11:58:01 -0700

    KeyedRegistrations class added to the Code Samples project to show case how to do keyed registrations in Simple Injector.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples/KeyedRegistrations.cs
@@ -0,0 +1,76 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class KeyedRegistrations<TKey, TService> : IEnumerable<TService> where TService : class
+    {
+        private static readonly Lifestyle DefaultLifestyle = Lifestyle.Transient;
+
+        private readonly List<InstanceProducer> producers = new List<InstanceProducer>();
+        private readonly Dictionary<TKey, InstanceProducer> keyedProducers;
+        private readonly Container container;
+
+        public KeyedRegistrations(Container container) : this(container, EqualityComparer<TKey>.Default)
+        {
+        }
+
+        public KeyedRegistrations(Container container, IEqualityComparer<TKey> comparer)
+        {
+            this.container = container;
+            this.keyedProducers = new Dictionary<TKey, InstanceProducer>(comparer);
+        }
+        
+        public TService GetInstance(TKey key)
+        {
+            return (TService)this.keyedProducers[key].GetInstance();
+        }
+
+        public void Register<TImplementation>(TKey key) where TImplementation : TService
+        {
+            this.Register(typeof(TImplementation), key);
+        }
+
+        public void Register(Type implementationType, TKey key)
+        {
+            this.Register(implementationType, key, DefaultLifestyle);
+        }
+
+        public void Register(Type implementationType, TKey key, Lifestyle lifestyle)
+        {
+            this.Register(lifestyle.CreateRegistration(typeof(TService), implementationType, this.container), key);
+        }
+        
+        public void Register(Func<TService> instanceCreator, TKey key)
+        {
+            this.Register(instanceCreator, key, DefaultLifestyle);
+        }
+
+        public void Register(Func<TService> instanceCreator, TKey key, Lifestyle lifestyle)
+        {
+            this.Register(lifestyle.CreateRegistration(typeof(TService), instanceCreator, this.container), key);
+        }
+
+        public IEnumerator<TService> GetEnumerator()
+        {
+            foreach (var producer in this.producers)
+            {
+                yield return (TService)producer.GetInstance();
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private void Register(Registration registration, TKey key)
+        {
+            var producer = new InstanceProducer(typeof(TService), registration);
+
+            this.keyedProducers.Add(key, producer);
+            this.producers.Add(producer);
+        }
+    }
+}
