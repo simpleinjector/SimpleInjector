f719a919e340941ad98c4f3b9f67957b34fc8342
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2013-07-24 12:09:12 -0700

    Missing tests for GetItem and SetItem methods added.

--- SimpleServiceLocator/SimpleInjector.NET.Tests.Unit/Advanced/AdvancedExtensionsTests.cs
+++ SimpleServiceLocator/SimpleInjector.NET.Tests.Unit/Advanced/AdvancedExtensionsTests.cs
@@ -300,6 +300,124 @@
                 action);
         }
 
+        [TestMethod]
+        public void GetItem_NoValueSet_ReturnsNull()
+        {
+            // Arrange
+            object key = new object();
+
+            var container = ContainerFactory.New();
+
+            // Act
+            object item = container.GetItem(key);
+
+            // Assert
+            Assert.IsNull(item);
+        }
+
+        [TestMethod]
+        public void GetItem_WithValueSet_ReturnsThatItem()
+        {
+            // Arrange
+            object key = new object();
+            object expectedItem = new object();
+
+            var container = ContainerFactory.New();
+
+            container.SetItem(key, expectedItem);
+
+            // Act
+            object actualItem = container.GetItem(key);
+
+            // Assert
+            Assert.AreSame(expectedItem, actualItem);
+        }
+
+        [TestMethod]
+        public void GetItem_WithValueSetInOneContainer_DoesNotReturnThatItemInAnotherContainer()
+        {
+            // Arrange
+            object key = new object();
+            object expectedItem = new object();
+
+            var container1 = ContainerFactory.New();
+            var container2 = ContainerFactory.New();
+
+            container1.SetItem(key, expectedItem);
+
+            // Act
+            object actualItem = container2.GetItem(key);
+
+            // Assert
+            Assert.IsNull(actualItem, "The items dictionary is expected to be container bound. Not static!");
+        }
+
+        [TestMethod]
+        public void GetItem_WithValueSetTwice_ReturnsLastItem()
+        {
+            // Arrange
+            object key = new object();
+            object firstItem = new object();
+            object expectedItem = new object();
+
+            var container = ContainerFactory.New();
+
+            container.SetItem(key, firstItem);
+            container.SetItem(key, expectedItem);
+
+            // Act
+            object actualItem = container.GetItem(key);
+
+            // Assert
+            Assert.AreSame(expectedItem, actualItem);
+        }
+
+        [TestMethod]
+        public void GetItem_WithValueReset_ReturnsNull()
+        {
+            // Arrange
+            object key = new object();
+
+            var container = ContainerFactory.New();
+
+            container.SetItem(key, new object());
+            container.SetItem(key, null);
+
+            // Act
+            object item = container.GetItem(key);
+
+            // Assert
+            // This test looks odd, but under the cover the item is removed from the collection when null
+            // is supplied to prevent the dictionary from ever increasing, but we have to test this code path.
+            Assert.IsNull(item, "When a value is overridden with null, it is expected to return null.");
+        }
+
+        [TestMethod]
+        public void GetItem_WithNullKey_ThrowsException()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            // Act
+            Action action = () => container.GetItem(null);
+
+            // Assert
+            AssertThat.Throws<ArgumentNullException>(action);
+        }
+
+        [TestMethod]
+        public void SetItem_WithNullKey_ThrowsException()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            // Act
+            Action action = () => container.SetItem(null, new object());
+
+            // Assert
+            AssertThat.Throws<ArgumentNullException>(action);
+        }
+
         private static Registration CreateRegistration(Container container)
         {
             return Lifestyle.Transient.CreateRegistration<IPlugin, PluginImpl>(container);
