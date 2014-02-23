f1811be51e46ffc579c6b4951c71a11a843b25d0
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2014-02-23 04:36:57 -0800

    Extension methods for registering automatic factories added to the CodeSamples project.

---
+++ SimpleServiceLocator/SimpleInjector.CodeSamples.Tests.Unit/AutomaticFactoryExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿namespace SimpleInjector.CodeSamples.Tests.Unit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AutomaticFactoryExtensionsTests
+    {
+        public interface IService
+        {
+        }
+
+        public interface IServiceFactory
+        {
+            IService CreateService();
+        }
+
+        [TestMethod]
+        public void GetInstance_TypeRegisteredAsFactory_ResolvesAsExpected()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.RegisterFactory<IServiceFactory>();
+
+            // Act
+            var factory = container.GetInstance<IServiceFactory>();
+
+            // Assert
+            Assert.IsNotNull(factory);
+        }
+
+        [TestMethod]
+        public void CallingFactoryMethod_OnInstanceResolvedAsAutomaticFactory_ResolvesExpectedService()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.Register<IService, Service>();
+
+            container.RegisterFactory<IServiceFactory>();
+
+            var factory = container.GetInstance<IServiceFactory>();
+
+            // Act
+            var instance = factory.CreateService();
+
+            // Assert
+            Assert.IsInstanceOfType(instance, typeof(Service));
+        }
+        
+        [TestMethod]
+        public void ToString_OnInstanceResolvedAsAutomaticFactory_Succeeds()
+        {
+            // Arrange
+            var container = new Container();
+
+            container.Register<IService, Service>();
+
+            container.RegisterFactory<IServiceFactory>();
+
+            var factory = container.GetInstance<IServiceFactory>();
+
+            // Act
+            factory.ToString();
+        }
+
+        public class Service : IService 
+        {
+        }
+    }
+}
