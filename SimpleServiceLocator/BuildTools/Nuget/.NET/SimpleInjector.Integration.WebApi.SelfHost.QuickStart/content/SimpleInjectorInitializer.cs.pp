adf0a515b6d3f523209264cd1154559aed068946
dot_NET_Junkie_cp <SND\dot_NET_Junkie_cp>
2014-03-15 13:44:29 -0700

    NuGet package templates updated to link at the new Simple Injector logo .ico file.

---
+++ SimpleServiceLocator/BuildTools/Nuget/.NET/SimpleInjector.Integration.WebApi.SelfHost.QuickStart/content/SimpleInjectorInitializer.cs.pp
@@ -0,0 +1,35 @@
+[assembly: WebActivator.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.SimpleInjectorWebApiInitializer), "Initialize")]
+
+namespace $rootnamespace$.App_Start
+{
+    using System.Web.Http;
+    using SimpleInjector;
+    using SimpleInjector.Integration.WebApi;
+    
+    public static class SimpleInjectorWebApiInitializer
+    {
+        /// <summary>Initialize the container and register it as MVC3 Dependency Resolver.</summary>
+        public static void Initialize()
+        {
+            // Did you know the container can diagnose your configuration? Go to: https://bit.ly/YE8OJj.
+            var container = new Container();
+            
+            InitializeContainer(container);
+
+            container.RegisterWebApiControllers(GlobalConfiguration.Configuration);
+       
+            container.Verify();
+            
+            GlobalConfiguration.Configuration.DependencyResolver =
+                new SimpleInjectorWebApiDependencyResolver(container);
+        }
+     
+        private static void InitializeContainer(Container container)
+        {
+#error Register your services here (remove this line).
+
+            // For instance:
+            // container.RegisterWebApiRequest<IUserRepository, SqlUserRepository>();
+        }
+    }
+}
