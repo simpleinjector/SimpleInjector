cf354f5985db1b3098831d8886dfde42582c69b7
.NET Junkie <steven@cuttingedge.it>
2014-07-06 18:52:48 +0200

    Typo fixed in XML comments.

--- SimpleInjector.NET/ScopedLifestyle.cs
+++ SimpleInjector.NET/ScopedLifestyle.cs
@@ -143,7 +143,7 @@ namespace SimpleInjector
         }
 
         /// <summary>
-        /// Creates a delegate that that upon invocation return the current <see cref="Scope"/> for this
+        /// Creates a delegate that upon invocation return the current <see cref="Scope"/> for this
         /// lifestyle and the given <paramref name="container"/>, or null when the delegate is executed outside
         /// the context of such scope.
         /// </summary>
