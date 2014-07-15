59216182a525ca7aa1740d6b3cbce2443c48e936
.NET Junkie <steven@cuttingedge.it>
2014-07-15 20:55:34 +0200

    Enabled batch-registering internal types in PCL library by including that part of the API as well. Work item 20396.

--- SimpleInjector.NET.Tests.Unit/Extensions/BatchRegistrationExtensionsTests.cs
+++ SimpleInjector.NET.Tests.Unit/Extensions/BatchRegistrationExtensionsTests.cs
@@ -647,6 +647,151 @@
             Assert_RegisterManyForOpenGenericWithCallback_ReturnsExpectedImplementations(types, expected);
         }
 
+        [TestMethod]
+        public void GetTypesToRegister3_Always_ReturnsAValue()
+        {
+            var result = OpenGenericBatchRegistrationExtensions.GetTypesToRegister(typeof(IService<,>),
+                AccessibilityOption.AllTypes, typeof(IService<,>).Assembly);
+
+            // Act
+            result.ToArray();
+        }
+
+        [TestMethod]
+        public void GetTypesToRegister4_Always_ReturnsAValue()
+        {
+            // Arrange
+            IEnumerable<Assembly> assemblies = new[] { typeof(IService<,>).Assembly };
+
+            var result = OpenGenericBatchRegistrationExtensions.GetTypesToRegister(typeof(IService<,>),
+                AccessibilityOption.AllTypes, assemblies);
+
+            // Act
+            result.ToArray();
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGenericAccessibilityOptionAndParams_WithValidTypeDefinitions_Succeeds()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            // Act
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), AccessibilityOption.PublicTypesOnly,
+                Assembly.GetExecutingAssembly());
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGenericAccessibilityOptionAndEnumerable_WithValidTypeDefinitions_Succeeds()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            IEnumerable<Assembly> assemblies = new[] { Assembly.GetExecutingAssembly() };
+
+            // Act
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), AccessibilityOption.PublicTypesOnly,
+                assemblies);
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGeneric_WithValidTypeDefinitions_RespectsTheSuppliedLifestyle1()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+            container.RegisterManyForOpenGeneric(typeof(IService<,>),
+                AccessibilityOption.PublicTypesOnly, Lifestyle.Transient,
+                Assembly.GetExecutingAssembly());
+
+            // Act
+            var impl1 = container.GetInstance<IService<int, string>>();
+            var impl2 = container.GetInstance<IService<int, string>>();
+
+            // Assert
+            Assert.AreNotEqual(impl1, impl2, "The types should be registered as transient.");
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGeneric_WithValidTypeDefinitions_RespectsTheSuppliedLifestyle2()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+            container.RegisterManyForOpenGeneric(typeof(IService<,>),
+                AccessibilityOption.PublicTypesOnly, Lifestyle.Singleton,
+                Assembly.GetExecutingAssembly());
+
+            // Act
+            var impl1 = container.GetInstance<IService<int, string>>();
+            var impl2 = container.GetInstance<IService<int, string>>();
+
+            // Assert
+            Assert.AreEqual(impl1, impl2, "The type should be returned as singleton.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RegisterManyForOpenGeneric_WithInvalidAccessibilityOption_ThrowsExpectedException()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            // Act
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), (AccessibilityOption)5,
+                Assembly.GetExecutingAssembly());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ActivationException))]
+        public void RegisterManyForOpenGeneric_ExcludingInternalTypes_DoesNotRegisterInternalTypes()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), AccessibilityOption.PublicTypesOnly,
+                Assembly.GetExecutingAssembly());
+
+            // Act
+            container.GetInstance<IService<decimal, decimal>>();
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGenericAssemblyIEnumerable_WithCallbackThatDoesNothing_DoesNotRegisterAnything()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            BatchRegistrationCallback callback = (closedServiceType, implementations) =>
+            {
+                // Do nothing.
+            };
+
+            IEnumerable<Assembly> assemblies = new[] { Assembly.GetExecutingAssembly() };
+
+            // Act
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), AccessibilityOption.PublicTypesOnly,
+                callback, assemblies);
+
+            // Assert
+            var registration = container.GetRegistration(typeof(IService<string, object>));
+
+            Assert.IsNull(registration, "GetRegistration should result in null, because by supplying a delegate, the " +
+                "extension method does not do any registration itself.");
+        }
+
+        [TestMethod]
+        public void RegisterManyForOpenGenericAccessibilityOptionCallbackEnum_WithValidArguments_Succeeds()
+        {
+            // Arrange
+            var container = ContainerFactory.New();
+
+            BatchRegistrationCallback callback = (closedServiceType, implementations) => { };
+
+            var assemblies = new[] { Assembly.GetExecutingAssembly() };
+
+            // Act
+            container.RegisterManyForOpenGeneric(typeof(IService<,>), AccessibilityOption.AllTypes, callback,
+                assemblies);
+        }
+
         private static void Assert_RegisterManyForOpenGenericWithCallback_ReturnsExpectedImplementations(
             Type[] inputTypes, Type[] expectedTypes)
         {
