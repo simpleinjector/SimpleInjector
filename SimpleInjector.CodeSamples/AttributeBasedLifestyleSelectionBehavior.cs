de6ddb2c8dd3b105aa4a4f8d0b8b8ee00f5f16b5
.NET Junkie <steven@cuttingedge.it>
2014-08-17 22:03:58 +0200

    Added example AttributeBasedLifestyleSelectionBehavior implementation.

---
+++ SimpleInjector.CodeSamples/AttributeBasedLifestyleSelectionBehavior.cs
@@ -0,0 +1,48 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Reflection;
+    using SimpleInjector.Advanced;
+
+    public enum CreationPolicy { Transient, Scoped, Singleton }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class CreationPolicyAttribute : Attribute
+    {
+        public CreationPolicyAttribute(CreationPolicy policy)
+        {
+            this.Policy = policy;
+        }
+
+        public CreationPolicy Policy { get; private set; }
+    }
+
+    public class AttributeBasedLifestyleSelectionBehavior : ILifestyleSelectionBehavior
+    {
+        private const CreationPolicy DefaultPolicy = CreationPolicy.Transient;
+        private readonly ScopedLifestyle scopedLifestyle;
+
+        public AttributeBasedLifestyleSelectionBehavior(ScopedLifestyle scopedLifestyle)
+        {
+            this.scopedLifestyle = scopedLifestyle;
+        }
+
+        public Lifestyle SelectLifestyle(Type serviceType, Type implementationType)
+        {
+            var attribute = implementationType.GetCustomAttribute<CreationPolicyAttribute>()
+                ?? serviceType.GetCustomAttribute<CreationPolicyAttribute>();
+
+            var policy = attribute == null ? DefaultPolicy : attribute.Policy;
+
+            switch (policy)
+            {
+                case CreationPolicy.Singleton:
+                    return Lifestyle.Singleton;
+                case CreationPolicy.Scoped:
+                    return this.scopedLifestyle;
+                default:
+                    return Lifestyle.Transient;
+            }
+        }
+    }
+}
