3c4ac8c8d55444a59808401a89602307a2177ef4
.NET Junkie <steven@cuttingedge.it>
2015-01-18 10:23:18 +0100

    In some special cases Simple Injector was not able to resolve generic types, where one of the generic type arguments was deducible from the others using the generic type constraints (work item 20994).

--- SimpleInjector.NET/Extensions/TypeConstraintValidator.cs
+++ SimpleInjector.NET/Extensions/TypeConstraintValidator.cs
@@ -91,18 +91,22 @@ namespace SimpleInjector.Extensions
         {
             var unsatisfiedConstraints =
                 from constraint in this.Mapping.Argument.GetGenericParameterConstraints()
-                where !this.MappingIsCompatibleWithTypeConstraint(constraint)
+                where !this.MappingMightBeCompatibleWithTypeConstraint(constraint)
                 select constraint;
 
             return !unsatisfiedConstraints.Any();
         }
 
-        private bool MappingIsCompatibleWithTypeConstraint(Type constraint)
+        private bool MappingMightBeCompatibleWithTypeConstraint(Type constraint)
         {
             // We return true in PCL, because there's no System.Type.GUID in PCL and GUID is needed to compare
             // if the constraint matches one of the base type. Returning true does not change the functional 
-            // behavior and correctness of the framework, but does lower the performance (especially during 
-            // a call to Verify).
+            // behavior and correctness of the framework, but does lower the performance when resolving a
+            // service for the first time. This can especially add up when calling Verify. In a benchmark of
+            // a complex application that made heavily use of generic registrations with type constraints,
+            // we've seen a call to Verify() take up to 6 times as long (from 8.5 seconds to 55 seconds), when 
+            // we don't do these checks here (and simply return true). 
+            // That's why we need to have these checks in the full version.
 #if PCL
             return true;
 #else
@@ -111,7 +115,7 @@ namespace SimpleInjector.Extensions
                 return true;
             }
 
-            if (constraint.IsGenericParameter)
+            if (constraint.ContainsGenericParameters)
             {
                 // The constraint is one of the other generic parameters, but this class checks a single
                 // mapping, so we cannot check whether this constraint holds. We just return true and
