cb49b77bf0ec9b2ad515661a1f90ab70fabbb7cf
dot_net_junkie <github@cuttingedge.it>
2015-10-03 20:05:54 +0200

    Added ExecutionContextScoping test project to build.

--- appveyor.bat
+++ appveyor.bat
@@ -5,6 +5,7 @@ if not exist SimpleInjector.snk copy fake.snk SimpleInjector.snk
 msbuild "SimpleInjector.NET\SimpleInjector.NET.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Packaging\SimpleInjector.Packaging.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Extensions.LifetimeScoping\SimpleInjector.Extensions.LifetimeScoping.csproj" /nologo /p:Configuration=Debug
+msbuild "SimpleInjector.Extensions.ExecutionContextScoping\SimpleInjector.Extensions.ExecutionContextScoping.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Integration.Web\SimpleInjector.Integration.Web.csproj" /nologo /p:Configuration=Debug
 rem doesn't build on AppVeyor. msbuild "SimpleInjector.Integration.Web.Mvc\SimpleInjector.Integration.Web.Mvc.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Integration.Wcf\SimpleInjector.Integration.Wcf.csproj" /nologo /p:Configuration=Debug
@@ -12,3 +13,4 @@ msbuild "SimpleInjector.Integration.Wcf\SimpleInjector.Integration.Wcf.csproj" /
 msbuild "SimpleInjector.NET.Tests.Unit\SimpleInjector.NET.Tests.Unit.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Extensions.LifetimeScoping.Tests.Unit\SimpleInjector.Extensions.LifetimeScoping.Tests.Unit.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Integration.Web.Tests.Unit\SimpleInjector.Integration.Web.Tests.Unit.csproj" /nologo /p:Configuration=Debug
+msbuild "SimpleInjector.Extensions.ExecutionContextScoping.Tests.Unit\SimpleInjector.Extensions.ExecutionContextScoping.Tests.Unit.csproj" /nologo /p:Configuration=Debug
