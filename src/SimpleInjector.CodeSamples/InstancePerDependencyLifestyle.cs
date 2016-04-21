bc3266092375e7a01958adc7722ab114625a25b7
dotnetjunkie <github@cuttingedge.it>
2016-04-21 16:18:25 +0200

    Code cleanup
    Cleanup of InstancePerDependencyLifestyle codesample.

--- src/SimpleInjector.CodeSamples/InstancePerDependencyLifestyle.cs
+++ src/SimpleInjector.CodeSamples/InstancePerDependencyLifestyle.cs
@@ -1,57 +1,44 @@
-﻿using System;
-using System.Linq.Expressions;
-
-namespace SimpleInjector.CodeSamples
+﻿namespace SimpleInjector.CodeSamples
 {
+    using System;
+    using System.Linq.Expressions;
+
     /// <summary>
     /// Injects a new instance for each dependency. Behaves the same as Transient but prevents any lifestyle
     /// mismatches from being reported. Use with care!!!
     /// </summary>
     public class InstancePerDependencyLifestyle : Lifestyle
     {
-        public InstancePerDependencyLifestyle() : base("Instance Per Dependency")
-        {
-        }
+        public InstancePerDependencyLifestyle() : base("Instance Per Dependency") { }
 
-        // Prevents lifestyle mismatches
+        // MaxValue prevents lifestyle mismatches
         protected override int Length => int.MaxValue;
 
-        protected override Registration CreateRegistrationCore<TService, TImplementation>(Container container) => 
-            new InstancePerDependencyLifestyleRegistration<TService, TImplementation>(this, container);
+        protected override Registration CreateRegistrationCore<TService, TImplementation>(Container c) => 
+            new InstancePerDependencyRegistration<TService, TImplementation>(this, c);
 
-        protected override Registration CreateRegistrationCore<TService>(Func<TService> instanceCreator,
-            Container container) => 
-            new InstancePerDependencyLifestyleRegistration<TService>(this, container, instanceCreator);
+        protected override Registration CreateRegistrationCore<TService>(Func<TService> ic, Container c) => 
+            new InstancePerDependencyRegistration<TService>(this, c, ic);
 
-        private sealed class InstancePerDependencyLifestyleRegistration<TService> : Registration
-            where TService : class
+        private sealed class InstancePerDependencyRegistration<TService> : Registration where TService : class
         {
-            private readonly Func<TService> instanceCreator;
+            private readonly Func<TService> ic;
 
-            public InstancePerDependencyLifestyleRegistration(Lifestyle lifestyle, Container container,
-                Func<TService> instanceCreator)
-                : base(lifestyle, container)
+            public InstancePerDependencyRegistration(Lifestyle l, Container c, Func<TService> ic) : base(l, c)
             {
-                this.instanceCreator = instanceCreator;
+                this.ic = ic;
             }
 
             public override Type ImplementationType => typeof(TService);
-
-            public override Expression BuildExpression() => this.BuildTransientExpression(this.instanceCreator);
+            public override Expression BuildExpression() => this.BuildTransientExpression(this.ic);
         }
 
-        private class InstancePerDependencyLifestyleRegistration<TService, TImplementation> : Registration
-            where TImplementation : class, TService
-            where TService : class
+        private class InstancePerDependencyRegistration<TService, TImpl> : Registration
+            where TImpl : class, TService where TService : class
         {
-            internal InstancePerDependencyLifestyleRegistration(Lifestyle lifestyle, Container container)
-                : base(lifestyle, container)
-            {
-            }
-
-            public override Type ImplementationType => typeof(TImplementation);
-
-            public override Expression BuildExpression() => this.BuildTransientExpression<TService, TImplementation>();
+            public InstancePerDependencyRegistration(Lifestyle l, Container c) : base(l, c) { }
+            public override Type ImplementationType => typeof(TImpl);
+            public override Expression BuildExpression() => this.BuildTransientExpression<TService, TImpl>();
         }
     }
 }
