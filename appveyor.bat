5e395d84769e5553f6262cb8abba2b485d32bdd9
NET Junkie <steven@cuttingedge.it>
2016-07-14 22:35:18 +0200

    Added msbuild for Integration.Web.Mvc project.

--- appveyor.bat
+++ appveyor.bat
@@ -7,7 +7,7 @@ msbuild "SimpleInjector.Packaging\SimpleInjector.Packaging.csproj" /nologo /p:Co
 msbuild "SimpleInjector.Extensions.LifetimeScoping\SimpleInjector.Extensions.LifetimeScoping.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Extensions.ExecutionContextScoping\SimpleInjector.Extensions.ExecutionContextScoping.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Integration.Web\SimpleInjector.Integration.Web.csproj" /nologo /p:Configuration=Debug
-rem doesn't build on AppVeyor. msbuild "SimpleInjector.Integration.Web.Mvc\SimpleInjector.Integration.Web.Mvc.csproj" /nologo /p:Configuration=Debug
+msbuild "SimpleInjector.Integration.Web.Mvc\SimpleInjector.Integration.Web.Mvc.csproj" /nologo /p:Configuration=Debug
 msbuild "SimpleInjector.Integration.Wcf\SimpleInjector.Integration.Wcf.csproj" /nologo /p:Configuration=Debug
 
 msbuild "SimpleInjector.NET.Tests.Unit\SimpleInjector.NET.Tests.Unit.csproj" /nologo /p:Configuration=Debug
