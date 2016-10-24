48cf6f3fcd03913dfa7453139c44060e4aa03881
NET Junkie <steven@cuttingedge.it>
2016-10-24 16:13:12 +0200

    Simplification in sample.
    Simplification in DisposableTransientLifestyle sample.

--- src/SimpleInjector.CodeSamples/DisposableTransientLifestyle.cs
+++ src/SimpleInjector.CodeSamples/DisposableTransientLifestyle.cs
@@ -2,13 +2,12 @@
 {
     using System;
     using System.Diagnostics;
-    using System.Globalization;
     using System.Linq.Expressions;
     using SimpleInjector.Advanced;
 
     /// <summary>
     /// Allows registering instances with a <see cref="Lifestyle.Transient">Transient</see> lifestyle, while
-    /// allowing them to get disposed on the boundary of a supplied <see cref="ScopedLifestyle"/>
+    /// allowing them to get disposed on the boundary of a supplied <see cref="ScopedLifestyle"/>.
     /// </summary>
     public class DisposableTransientLifestyle : Lifestyle
     {
@@ -21,11 +20,8 @@
             this.scopedLifestyle = scopedLifestyle;
         }
 
-        protected override int Length
-        {
-            // Same length as Transient.
-            get { return 1; }
-        }
+        // Same length as Transient.
+        protected override int Length => 1;
 
         [DebuggerStepThrough]
         public static void EnableForContainer(Container container)
@@ -45,8 +41,7 @@
         {
             TryEnableTransientDisposalOrThrow(container);
 
-            var registration = 
-                new DisposableTransientLifestyleRegistration<TService, TImplementation>(this, container);
+            var registration = new DisposableTransientLifestyleRegistration<TService, TImplementation>(this, container);
 
             registration.ScopedLifestyle = this.scopedLifestyle;
 
@@ -54,7 +49,7 @@
         }
 
         [DebuggerStepThrough]
-        protected override Registration CreateRegistrationCore<TService>(Func<TService> instanceCreator, 
+        protected override Registration CreateRegistrationCore<TService>(Func<TService> instanceCreator,
             Container container)
         {
             TryEnableTransientDisposalOrThrow(container);
@@ -97,32 +92,13 @@
                 if (instance != null)
                 {
                     var registation = (DisposableTransientRegistration)data.Context.Registration;
-
-                    var scope = registation.ScopedLifestyle.GetCurrentScope(container);
-
-                    if (scope == null)
-                    {
-                        ThrowOutsideScopeException(data.Context.Producer.ServiceType, registation);
-                    }
-
-                    scope.RegisterForDisposal(instance);
+                    registation.ScopedLifestyle.RegisterForDisposal(container, instance);
                 }
             };
 
             container.RegisterInitializer(instanceInitializer, shouldRunInstanceInitializer);
         }
 
-        private static void ThrowOutsideScopeException(Type serviceType,
-            DisposableTransientRegistration registation)
-        {
-            throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture,
-                "The {0} is registered as '{1}' lifestyle, but the instance is requested " +
-                "outside the context of a {2}.",
-                serviceType.FullName,
-                registation.Lifestyle.Name,
-                registation.ScopedLifestyle.Name));
-        }
-        
         private abstract class DisposableTransientRegistration : Registration
         {
             protected DisposableTransientRegistration(Lifestyle lifestyle, Container container)
@@ -133,31 +109,25 @@
             internal ScopedLifestyle ScopedLifestyle { get; set; }
         }
 
-        private sealed class DisposableTransientLifestyleRegistration<TService> 
+        private sealed class DisposableTransientLifestyleRegistration<TService>
             : DisposableTransientRegistration
             where TService : class
         {
             private readonly Func<TService> instanceCreator;
 
-            internal DisposableTransientLifestyleRegistration(Lifestyle lifestyle, Container container, 
+            internal DisposableTransientLifestyleRegistration(Lifestyle lifestyle, Container container,
                 Func<TService> instanceCreator)
                 : base(lifestyle, container)
             {
                 this.instanceCreator = instanceCreator;
             }
 
-            public override Type ImplementationType
-            {
-                get { return typeof(TService); }
-            }
+            public override Type ImplementationType => typeof(TService);
 
-            public override Expression BuildExpression()
-            {
-                return this.BuildTransientExpression<TService>(this.instanceCreator);
-            }
+            public override Expression BuildExpression() => this.BuildTransientExpression(this.instanceCreator);
         }
 
-        private class DisposableTransientLifestyleRegistration<TService, TImplementation> 
+        private class DisposableTransientLifestyleRegistration<TService, TImplementation>
             : DisposableTransientRegistration
             where TImplementation : class, TService
             where TService : class
@@ -167,15 +137,10 @@
             {
             }
 
-            public override Type ImplementationType
-            {
-                get { return typeof(TImplementation); }
-            }
+            public override Type ImplementationType => typeof(TImplementation);
 
-            public override Expression BuildExpression()
-            {
-                return this.BuildTransientExpression<TService, TImplementation>();
-            }
+            public override Expression BuildExpression() =>
+                this.BuildTransientExpression<TService, TImplementation>();
         }
     }
 }
