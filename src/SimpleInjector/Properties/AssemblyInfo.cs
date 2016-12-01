f3bb5a825d03f34c6b934e53fcda072628332a57
NET Junkie <steven@cuttingedge.it>
2016-12-01 21:51:41 +0100

    Simplified build process
    Building was not possible when the .snk was missing (which basically
    means only the Contributors could build Simple Injector). Upon prebuild
    the fake.snk is now copied to SimpleInjector.snk in case the real .snk
    is missing. This allows the project to be built without problems.

--- src/SimpleInjector/Properties/AssemblyInfo.cs
+++ src/SimpleInjector/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Runtime.CompilerServices;
 
 #if !PUBLISH
-[assembly: InternalsVisibleTo("SimpleInjector.Core.Tests.Unit, PublicKey=" + 
-    "0024000004800000940000000602000000240000525341310004000001000100d9fe51dff71d3f" +
-    "dcf9bfcccf7b4f589f530449a7414aec14b3d08abdde4229eea5a42f5636c738272c44e07dad2a" +
-    "92a1186525779360997c6e0e6153d6d8b1d25c7fe9359b2d230530e7ccccd02a32269ce22e4f1a" +
-    "a313cd995f6ee682c88b24acf8e6c9f6ddc95094eaeafe39e626b3765fd9b4f2e7789c3a6ed1c4" +
-    "a66dedb9")]
+[assembly: InternalsVisibleTo("SimpleInjector.Core.Tests.Unit, PublicKey=" +
+    "0024000004800000940000000602000000240000525341310004000001000100b3b15f4838ebbf" +
+    "7da8c0aafd638ecaf0727d70194da928e9ecc56889c1f1bfff78ec012f64e9f23602ae0741ea5f" +
+    "d3761507c4bea021b7503315a6e95ce620dc01d8082afef6a3282b45c2e48f298f8bed3d1b75af" +
+    "2b32e03a986a32da4b64ed2832b8254c7dbe723c6e412d02fb8f94603e6140237fddd4d9c2cb29" +
+    "05cc27d4")]
 #endif
