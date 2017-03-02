4d6ddbe5c7984cdb675931309ba2d5ba5ef19227
NET Junkie <steven@cuttingedge.it>
2017-03-02 15:55:43 +0100

    Upgraded to RportGenerator 2.5.3.

--- src/runcoverage.bat
+++ src/runcoverage.bat
@@ -4,6 +4,6 @@ cd SimpleInjector.Core.Tests.Unit
 
 ..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -mergeoutput -register:user -excludebyattribute:*.ExcludeFromCodeCoverage*^ "-target:C:\Program Files\dotnet\dotnet.exe" -targetargs:test -filter:"+[*]SimpleInjector.* -[*.Tests.*]* -[*.CodeSamples*]*" -output:..\coverage\coverage.xml -oldStyle
 
-..\packages\ReportGenerator.2.5.1\tools\ReportGenerator.exe "-reports:..\coverage\coverage.xml" "-targetdir:..\coverage\report" "-filters:-*.Tests*;" "-historydir:coverage\history"
+..\packages\ReportGenerator.2.5.3\tools\ReportGenerator.exe "-reports:..\coverage\coverage.xml" "-targetdir:..\coverage\report" "-filters:-*.Tests*;" "-historydir:coverage\history"
 
 ..\coverage\report\index.htm
