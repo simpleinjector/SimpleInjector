d8948fdd68f7712de53bd569dd6aeee1f597c203
Alex Batishchev <alexbat@microsoft.com>
2017-08-18 10:44:17 -0700

    Adding batch Register() overload accepting params Asesmbly[]

--- src/SimpleInjector/Container.Registration.Batch.cs
+++ src/SimpleInjector/Container.Registration.Batch.cs
@@ -53,6 +53,29 @@ namespace SimpleInjector
         /// <exception cref="InvalidOperationException">Thrown when the given set of 
         /// <paramref name="assemblies"/> contain multiple types that implement the same 
         /// closed generic version of the given <paramref name="openGenericServiceType"/>.</exception>
+        public void Register(Type openGenericServiceType, params Assembly[] assemblies)
+        {
+            this.Register(openGenericServiceType, assemblies, this.SelectionBasedLifestyle);
+        }
+
+        /// <summary>
+        /// Registers all concrete, non-generic, public and internal types in the given set of
+        /// <paramref name="assemblies"/> that implement the given <paramref name="openGenericServiceType"/> 
+        /// with container's default lifestyle (which is transient by default).
+        /// <see cref="TypesToRegisterOptions.IncludeDecorators">Decorators</see> and
+        /// <see cref="TypesToRegisterOptions.IncludeGenericTypeDefinitions">generic type definitions</see>
+        /// will be excluded from registration, while 
+        /// <see cref="TypesToRegisterOptions.IncludeComposites">composites</see> are included.
+        /// </summary>
+        /// <param name="openGenericServiceType">The definition of the open generic type.</param>
+        /// <param name="assemblies">A list of assemblies that will be searched.</param>
+        /// <exception cref="ArgumentNullException">Thrown when one of the arguments contain a null
+        /// reference (Nothing in VB).</exception>
+        /// <exception cref="ArgumentException">Thrown when <paramref name="openGenericServiceType"/> is not
+        /// an open generic type.</exception>
+        /// <exception cref="InvalidOperationException">Thrown when the given set of 
+        /// <paramref name="assemblies"/> contain multiple types that implement the same 
+        /// closed generic version of the given <paramref name="openGenericServiceType"/>.</exception>
         public void Register(Type openGenericServiceType, IEnumerable<Assembly> assemblies)
         {
             this.Register(openGenericServiceType, assemblies, this.SelectionBasedLifestyle);
