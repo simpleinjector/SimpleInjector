c3726f2a0d4262cf9f95880e70a449433e7f434d
NET Junkie <steven@cuttingedge.it>
2017-09-19 09:30:45 +0200

    AutoResolveUnregisteresTypes example added.

---
+++ src/SimpleInjector.CodeSamples/AutoResolveUnregisteredTypeExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleInjector.CodeSamples2
+{
+    using System.Linq;
+    using System.Reflection;
+
+    public static class AutoResolveUnregisteredTypeExtensions
+    {
+        /// <summary>
+        /// Automatically resolves an implementation of an unregistered abstraction, by searching the supplied
+        /// list of assemblies. The implementation will be resolved only if the list of assemblies contains
+        /// exactly one implementation of the missing abstraction.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <param name="assemblies">The list of assemblies to search for implementations.</param>
+        public static void AutoResolveUnregisteresTypes(this Container container, params Assembly[] assemblies)
+        {
+            container.ResolveUnregisteredType += (s, e) =>
+            {
+                if (e.UnregisteredServiceType.IsAbstract)
+                {
+                    var types = container.GetTypesToRegister(e.UnregisteredServiceType, assemblies).ToArray();
+
+                    // Only map when there is no ambiguity, meaning: exactly one implementation.
+                    if (types.Length == 1)
+                    {
+                        e.Register(container.Options.DefaultLifestyle.CreateRegistration(types[0], container));
+                    }
+                }
+            };
+        }
+    }
+}
