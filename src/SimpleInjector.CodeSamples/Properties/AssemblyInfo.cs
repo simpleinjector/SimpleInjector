f47635eabe91dcfd4c916acfc2d674616db09487
NET Junkie <steven@cuttingedge.it>
2017-09-25 22:16:34 +0200

    Fixed namespace (part 2).

--- src/SimpleInjector.CodeSamples/Properties/AssemblyInfo.cs
+++ src/SimpleInjector.CodeSamples/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SimpleInjector.CodeSamples2")]
+[assembly: AssemblyProduct("SimpleInjector.CodeSamples")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
