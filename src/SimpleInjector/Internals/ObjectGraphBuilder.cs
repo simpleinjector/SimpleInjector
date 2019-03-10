bf953c89098803797542f2c6d342e2bc22037588
Ka-Wai Lin <kwlin@users.noreply.github.com>
2019-03-10 18:38:21 +0100

    Copyright added on new files

--- src/SimpleInjector/Internals/ObjectGraphBuilder.cs
+++ src/SimpleInjector/Internals/ObjectGraphBuilder.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Copyright Simple Injector Contributors
+/* The Simple Injector is an easy-to-use Inversion of Control library for .NET
+ * 
+ * Copyright (c) 2013 - 2015 Simple Injector Contributors
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including 
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
+ * following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or substantial 
+ * portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
+ * LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO 
+ * EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#endregion
 
 namespace SimpleInjector.Internals
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     internal sealed class ObjectGraphBuilder
     {
-        private readonly StringBuilder builder = new StringBuilder(100);
+        private readonly StringBuilder builder = new StringBuilder();
         private readonly Stack<ProducerEntry> producers = new Stack<ProducerEntry>();
         private readonly bool writeLifestyles;
 
@@ -23,7 +45,7 @@ namespace SimpleInjector.Internals
 
         internal void BeginInstanceProducer(InstanceProducer producer)
         {
-            if (this.producers.Any())
+            if (this.producers.Count > 0)
             {
                 this.AppendLifestyle(this.producers.Peek());
                 this.AppendNewLine();
