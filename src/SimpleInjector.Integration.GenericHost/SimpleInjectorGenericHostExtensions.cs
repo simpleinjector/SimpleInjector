f6cdf8705bae3371ee7eccf22b98cd910399ee9a
dotnetjunkie <steven@cuttingedge.it>
2019-05-08 14:57:20 +0200

    Added IHost.UseSimpleInjector() extension method. #639

--- src/SimpleInjector.Integration.GenericHost/SimpleInjectorGenericHostExtensions.cs
+++ src/SimpleInjector.Integration.GenericHost/SimpleInjectorGenericHostExtensions.cs
@@ -20,7 +20,7 @@
 */
 #endregion
 
-namespace SimpleInjector.Integration.GenericHost
+namespace SimpleInjector
 {
     using System;
     using Microsoft.Extensions.DependencyInjection;
@@ -59,6 +59,38 @@ namespace SimpleInjector.Integration.GenericHost
                 _ => options.Container.GetInstance<THostedService>());
 
             return options;
+        }
+
+        /// <summary>
+        /// Finalizes the configuration of Simple Injector on top of <see cref="IHost"/>.
+        /// Ensures framework components can be injected into Simple Injector-resolved components, unless
+        /// <see cref="SimpleInjectorUseOptions.AutoCrossWireFrameworkComponents"/> is set to <c>false</c>
+        /// using the <paramref name="setupAction"/>.
+        /// </summary>
+        /// <param name="host">The application's <see cref="IHost"/>.</param>
+        /// <param name="container">The application's <see cref="Container"/> instance.</param>
+        /// <param name="setupAction">An optional setup action.</param>
+        /// <returns>The supplied <paramref name="host"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="host"/> or
+        /// <paramref name="container"/> are null references.</exception>
+        public static IHost UseSimpleInjector(
+            this IHost host,
+            Container container,
+            Action<SimpleInjectorUseOptions> setupAction = null)
+        {
+            if (host is null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            if (container is null)
+            {
+                throw new ArgumentNullException(nameof(container));
+            }
+
+            host.Services.UseSimpleInjector(container, setupAction);
+
+            return host;
         }
     }
 }
