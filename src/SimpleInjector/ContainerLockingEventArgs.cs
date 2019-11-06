ae5718e27da0cff5b2764132918ac64f72fc8e57
dotnetjunkie <steven@cuttingedge.it>
2019-11-06 21:25:17 +0100

    Added a Container.Options.ContainerLocking event to act on the locking of the container. Fixes #767.

---
+++ src/SimpleInjector/ContainerLockingEventArgs.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Simple Injector Contributors. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
+namespace SimpleInjector
+{
+    using System;
+
+    /// <summary>
+    /// Provides data for and interaction with the
+    /// <see cref="ContainerOptions.ContainerLocking">ContainerLocking</see> event of
+    /// the <see cref="ContainerOptions"/>.
+    /// </summary>
+    public class ContainerLockingEventArgs : EventArgs
+    {
+        internal ContainerLockingEventArgs()
+        {
+        }
+    }
+}
