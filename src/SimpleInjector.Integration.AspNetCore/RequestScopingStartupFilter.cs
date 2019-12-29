3ed14b40acdbf7b30fa84e9fa69b747f30c1069b
steven <steven@cuttingedge.it>
2019-12-29 12:23:48 +0100

    Scoped IAsyncDisposable components are now automatically (asynchronously) disposed of at the end of the web request. Fixes #778

--- src/SimpleInjector.Integration.AspNetCore/RequestScopingStartupFilter.cs
+++ src/SimpleInjector.Integration.AspNetCore/RequestScopingStartupFilter.cs
@@ -4,9 +4,11 @@
 namespace SimpleInjector.Integration.AspNetCore
 {
     using System;
-    using Lifestyles;
+    using System.Threading.Tasks;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using SimpleInjector;
+    using SimpleInjector.Lifestyles;
 
     internal sealed class RequestScopingStartupFilter : IStartupFilter
     {
@@ -29,13 +31,30 @@ namespace SimpleInjector.Integration.AspNetCore
 
         private void ConfigureRequestScoping(IApplicationBuilder builder)
         {
-            builder.Use(async (context, next) =>
+            builder.Use(async (_, next) =>
             {
-                using (AsyncScopedLifestyle.BeginScope(this.container))
+                using (var scope = AsyncScopedLifestyle.BeginScope(this.container))
                 {
                     await next();
+
+                    await DisposeAsync(scope);
                 }
             });
+        }
+
+        private static async ValueTask DisposeAsync(Scope scope)
+        {
+            IDisposable[] disposables = scope.GetDisposables();
+
+            // Dispose in reverse order
+            for (int index = disposables.Length - 1; index >= 0; index--)
+            {
+                if (disposables[index] is IAsyncDisposable asyncDisposable)
+                {
+                    // We don't need to catch-and-continue; Scope will guarantee everything is disposed.
+                    await asyncDisposable.DisposeAsync();
+                }
+            }
         }
     }
 }
