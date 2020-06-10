0e9f95286502cfc1bfcc5d8909aadf1e4fb1f60a
dotnetjunkie <steven@cuttingedge.it>
2020-06-10 10:32:00 +0200

    Prevented new unneeded object to be creatred.

--- src/SimpleInjector.DynamicAssemblyCompilation/DynamicAssemblyCompilationExtensions.cs
+++ src/SimpleInjector.DynamicAssemblyCompilation/DynamicAssemblyCompilationExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Simple Injector Contributors. All rights reserved.
 // Licensed under the MIT License. See LICENSE file in the project root for license information.
 
+using System;
+using SimpleInjector.Advanced;
 using SimpleInjector.DynamicAssemblyCompilation;
 
 namespace SimpleInjector
@@ -10,11 +12,19 @@ namespace SimpleInjector
     /// </summary>
     public static class DynamicAssemblyCompilationExtensions
     {
+        private static readonly IExpressionCompilationBehavior Behavior =
+            new DynamicAssemblyExpressionCompilationBehavior();
+
         /// <summary>Enables dynamic assembly compilation.</summary>
         /// <param name="options">The container options.</param>
         public static void EnableDynamicAssemblyCompilation(this ContainerOptions options)
         {
-            options.ExpressionCompilationBehavior = new DynamicAssemblyExpressionCompilationBehavior();
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            options.ExpressionCompilationBehavior = Behavior;
         }
     }
 }
