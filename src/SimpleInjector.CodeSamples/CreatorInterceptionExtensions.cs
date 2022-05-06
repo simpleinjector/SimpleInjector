c144696facd45d4230605a379ccd8b367861ced6
dotnetjunkie <steven@cuttingedge.it>
2022-05-06 18:41:19 +0200

    Added ApplyInterceptor extension method to the code samples project.

---
+++ src/SimpleInjector.CodeSamples/CreatorInterceptionExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace SimpleInjector.CodeSamples
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public static class CreatorInterceptionExtensions
+    {
+        public static void ApplyInterceptor(
+            this ContainerOptions options,
+            Func<Func<object>, object> interceptor,
+            Predicate<Type> predicate = null)
+        {
+            var container = options.Container;
+
+            container.ExpressionBuilding += (s, e) =>
+            {
+                if (predicate != null && !predicate(e.KnownImplementationType)) return;
+
+                Delegate factory = Expression.Lambda(typeof(Func<object>), e.Expression).Compile();
+
+                e.Expression = Expression.Convert(
+                    Expression.Invoke(
+                        Expression.Constant(interceptor),
+                        Expression.Constant(factory)),
+                    e.Expression.Type);
+
+            };
+        }
+    }
+}
